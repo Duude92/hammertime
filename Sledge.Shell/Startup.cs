92d140d5d11c38165e3744cc1bb5eea0cb3d4ff6
Duude92 <switch4acc@gmail.com>
2024-02-08 19:09:08 +0200

    Added startup exception log

--- Sledge.Shell/Startup.cs
+++ Sledge.Shell/Startup.cs
@@ -1,77 +1,89 @@
 using System;
 using System.ComponentModel;
 using System.ComponentModel.Composition.Hosting;
+using System.IO;
 using System.Windows.Forms;
 using LogicAndTrick.Oy;
 using Sledge.Common;
 
 namespace Sledge.Shell
 {
-    /// <summary>
-    /// Class to bootstrap the shell
-    /// </summary>
-    public static class Startup
-    {
-        /// <summary>
-        /// Called before the composition container is created, but after the default catalog has been created.
-        /// </summary>
-        public static event EventHandler<AggregateCatalog> BuildCatalog;
+	/// <summary>
+	/// Class to bootstrap the shell
+	/// </summary>
+	public static class Startup
+	{
+		/// <summary>
+		/// Called before the composition container is created, but after the default catalog has been created.
+		/// </summary>
+		public static event EventHandler<AggregateCatalog> BuildCatalog;
 
-        /// <summary>
-        /// Run the shell as an application using a container from the application catalog
-        /// </summary>
-        public static void Run()
-        {
-            var catalog = new AggregateCatalog();
-            
-            catalog.Catalogs.Add(new ValidAssembliesInDirectoryContainer(AppDomain.CurrentDomain.BaseDirectory));
-            BuildCatalog?.Invoke(null, catalog);
+		/// <summary>
+		/// Run the shell as an application using a container from the application catalog
+		/// </summary>
+		public static void Run()
+		{
+			try
+			{
+				var catalog = new AggregateCatalog();
 
-            var container = new CompositionContainer(catalog, CompositionOptions.DisableSilentRejection);
+				catalog.Catalogs.Add(new ValidAssembliesInDirectoryContainer(AppDomain.CurrentDomain.BaseDirectory));
+				BuildCatalog?.Invoke(null, catalog);
 
-            Run(container);
-        }
+				var container = new CompositionContainer(catalog, CompositionOptions.DisableSilentRejection);
 
-        /// <summary>
-        /// Run the shell with a custom container
-        /// </summary>
-        /// <param name="container">The container</param>
-        public static void Run(CompositionContainer container)
-        {
-            Common.Container.Initialise(container);
+				Run(container);
+			}
+			catch (Exception e)
+			{
+				using (StreamWriter outputFile = new StreamWriter("./startup.log"))
+				{
+					outputFile.WriteLine(e.ToString());
+					outputFile.Write(e.Message);
+				}
+			}
+		}
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            
-            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
-            Application.ThreadException += (s, e) => UnhandledException(e.Exception);
-            AppDomain.CurrentDomain.UnhandledException += (s, e) => UnhandledException(e.ExceptionObject as Exception);
+		/// <summary>
+		/// Run the shell with a custom container
+		/// </summary>
+		/// <param name="container">The container</param>
+		public static void Run(CompositionContainer container)
+		{
+			Common.Container.Initialise(container);
 
-            Oy.UnhandledException += (s, e) => UnhandledException(e.Exception);
-            
-            var shell = container.GetExport<Forms.Shell>().Value;
-            var si = new SingleInstance(shell);
-            
-            si.UnhandledException += (s, e) =>
-            {
-                e.ExitApplication = false;
-                UnhandledException(e.Exception);
-            };
-            
-            si.Run(Environment.GetCommandLineArgs());
-        }
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
 
-        private static void UnhandledException(Exception ex)
-        {
-            if (ex == null) return;
-            try
-            {
-                Oy.Publish("Shell:UnhandledException", ex);
-            }
-            catch
-            {
-                // Exception in an exception? Oh dear...
-            }
-        }
-    }
+			Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+			Application.ThreadException += (s, e) => UnhandledException(e.Exception);
+			AppDomain.CurrentDomain.UnhandledException += (s, e) => UnhandledException(e.ExceptionObject as Exception);
+
+			Oy.UnhandledException += (s, e) => UnhandledException(e.Exception);
+
+			var shell = container.GetExport<Forms.Shell>().Value;
+			var si = new SingleInstance(shell);
+
+			si.UnhandledException += (s, e) =>
+			{
+				e.ExitApplication = false;
+				UnhandledException(e.Exception);
+			};
+
+			si.Run(Environment.GetCommandLineArgs());
+		}
+
+		private static void UnhandledException(Exception ex)
+		{
+			if (ex == null) return;
+			try
+			{
+				Oy.Publish("Shell:UnhandledException", ex);
+			}
+			catch
+			{
+				// Exception in an exception? Oh dear...
+			}
+		}
+	}
 }
