44ecb7f30b073b641a9d1c52a5da8f45c191ce93
Duude92 <switch4acc@gmail.com>
2024-02-08 09:36:37 +0200

    Updated project to .net8, need to test, possibilly could have issues with older windows systems

--- Sledge.BspEditor.Editing/Components/Properties/ObjectPropertiesDialog.cs
+++ Sledge.BspEditor.Editing/Components/Properties/ObjectPropertiesDialog.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
 using System.Linq;
-using System.Runtime.Remoting.Channels;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using LogicAndTrick.Oy;
