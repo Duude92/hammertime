b2fbcbb898ed5e5915c628d7a81ac7cb399d35d6
Duude92 <switch4acc@gmail.com>
2024-04-20 15:24:16 +0300

    Added feature to download right version of update from current branch

--- Sledge.Editor/Update/UpdateReleaseDetails.cs
+++ Sledge.Editor/Update/UpdateReleaseDetails.cs
@@ -17,12 +17,12 @@ namespace Sledge.Editor.Update
 
         public bool Exists => Tag != null;
 
-        public UpdateReleaseDetails(string jsonString)
+        public UpdateReleaseDetails(string jsonString, string tag)
         {
             var obj = JsonConvert.DeserializeObject(jsonString) as JArray;
             if (obj == null || obj.Count < 1) return;
 
-			var rel = obj.Select(x => x as JObject).Where(x => x.GetValue("tag_name").ToString().Equals("latest")).FirstOrDefault(); //find the "latest" tagged asset
+            var rel = obj.Select(x => x as JObject).Where(x => x.GetValue("tag_name").ToString().Equals(tag)).FirstOrDefault();
 
             var assets = rel?.GetValue("assets") as JArray;
             if (assets == null || assets.Count < 1) return;
