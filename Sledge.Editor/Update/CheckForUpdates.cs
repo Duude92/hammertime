6ba778eda9a6aeb802eda227453bf2b420702faa
Duude92 <switch4acc@gmail.com>
2024-04-30 19:43:14 +0300

    New approach to merge update channels, last .net6 version would update from 'latest' channel

--- Sledge.Editor/Update/CheckForUpdates.cs
+++ Sledge.Editor/Update/CheckForUpdates.cs
@@ -69,7 +69,7 @@ namespace Sledge.Editor.Update
 				}
 				return;
 			}
-			if (details.PublishDate < (buildInfo.BuildTime.AddMinutes(15)))
+			if (details.PublishDate < (buildInfo.BuildTime.AddDays(-15)))
 			{
 				if (!silent)
 				{
