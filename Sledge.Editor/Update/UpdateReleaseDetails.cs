e401c122c6d6dd831639436fb9d8115c8ca18f91
Duude92 <switch4acc@gmail.com>
2024-05-20 16:28:34 +0300

    Added feature to download latest build if branch build not found

--- Sledge.Editor/Update/UpdateReleaseDetails.cs
+++ Sledge.Editor/Update/UpdateReleaseDetails.cs
@@ -6,36 +6,38 @@ using Newtonsoft.Json.Linq;
 
 namespace Sledge.Editor.Update
 {
-    public class UpdateReleaseDetails
-    {
-        public string Tag { get; }
-        public string Name { get; }
-        public string Changelog { get; }
-        public string FileName { get; }
-        public string DownloadUrl { get; }
-        public DateTime PublishDate { get; }
+	public class UpdateReleaseDetails
+	{
+		public string Tag { get; }
+		public string Name { get; }
+		public string Changelog { get; }
+		public string FileName { get; }
+		public string DownloadUrl { get; }
+		public DateTime PublishDate { get; }
 
-        public bool Exists => Tag != null;
+		public bool Exists => Tag != null;
 
-        public UpdateReleaseDetails(string jsonString, string tag)
-        {
-            var obj = JsonConvert.DeserializeObject(jsonString) as JArray;
-            if (obj == null || obj.Count < 1) return;
+		public UpdateReleaseDetails(string jsonString, string tag)
+		{
+			var obj = JsonConvert.DeserializeObject(jsonString) as JArray;
+			if (obj == null || obj.Count < 1) return;
 
-            var rel = obj.Select(x => x as JObject).Where(x => x.GetValue("tag_name").ToString().Equals(tag)).FirstOrDefault();
+			var rel = obj.Select(x => x as JObject).Where(x => x.GetValue("tag_name").ToString().Equals(tag)).FirstOrDefault();
+			// If tag not found - download latest tag
+			rel = rel ?? obj.Select(x => x as JObject).Where(x => x.GetValue("tag_name").ToString().Equals("latest")).FirstOrDefault();
 
-            var assets = rel?.GetValue("assets") as JArray;
-            if (assets == null || assets.Count < 1) return;
+			var assets = rel?.GetValue("assets") as JArray;
+			if (assets == null || assets.Count < 1) return;
 
-            var exeAsset = assets.FirstOrDefault(x => x is JObject o && o.GetValue("name").ToString().EndsWith(".exe")) as JObject;
-            if (exeAsset == null) return;
+			var exeAsset = assets.FirstOrDefault(x => x is JObject o && o.GetValue("name").ToString().EndsWith(".exe")) as JObject;
+			if (exeAsset == null) return;
 
-            Tag = rel.GetValue("tag_name").ToString();
-            Name = rel.GetValue("name").ToString();
-            Changelog = rel.GetValue("body").ToString();
-            FileName = exeAsset.GetValue("name").ToString();
-            DownloadUrl = exeAsset.GetValue("url").ToString();
+			Tag = rel.GetValue("tag_name").ToString();
+			Name = rel.GetValue("name").ToString();
+			Changelog = rel.GetValue("body").ToString();
+			FileName = exeAsset.GetValue("name").ToString();
+			DownloadUrl = exeAsset.GetValue("url").ToString();
 			PublishDate = exeAsset.GetValue("created_at").Value<DateTime>();
 		}
-    }
+	}
 }
