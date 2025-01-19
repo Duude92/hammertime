1f5bfc651ee842a1d203191a3afeb93d7195b6a1
Duude92 <switch4acc@gmail.com>
2025-01-19 20:12:45 +0200

    Added BillboardUV struct to pass frame information to animated Sprite shader

---
+++ Sledge.Rendering/Primitives/BillboardUV.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sledge.Rendering.Primitives
+{
+    public struct BillboardUV
+    {
+        public float FrameCount;
+        public float CurrentFrame;
+        public Vector2 UniformPadding;
+    }
+}
