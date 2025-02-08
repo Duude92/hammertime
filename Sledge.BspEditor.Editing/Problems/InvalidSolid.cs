ab83d85e041ff9159702264239f301a5e8942d07
Duude92 <switch4acc@gmail.com>
2025-02-08 14:32:01 +0200

    Fix "InvalidSolid" problem solver: recreate new solid based on previous

--- Sledge.BspEditor.Editing/Problems/InvalidSolid.cs
+++ Sledge.BspEditor.Editing/Problems/InvalidSolid.cs
@@ -7,40 +7,65 @@ using Sledge.BspEditor.Documents;
 using Sledge.BspEditor.Modification;
 using Sledge.BspEditor.Modification.Operations.Tree;
 using Sledge.BspEditor.Primitives;
+using Sledge.BspEditor.Primitives.MapObjectData;
 using Sledge.BspEditor.Primitives.MapObjects;
 using Sledge.Common.Translations;
+using Sledge.DataStructures.Geometric;
 
 namespace Sledge.BspEditor.Editing.Problems
 {
-    [Export(typeof(IProblemCheck))]
-    [AutoTranslate]
-    public class InvalidSolid : IProblemCheck
-    {
-        public string Name { get; set; }
-        public string Details { get; set; }
-        public Uri Url => null;
-        public bool CanFix => true;
+	[Export(typeof(IProblemCheck))]
+	[AutoTranslate]
+	public class InvalidSolid : IProblemCheck
+	{
+		public string Name { get; set; }
+		public string Details { get; set; }
+		public Uri Url => null;
+		public bool CanFix => true;
 
-        public Task<List<Problem>> Check(MapDocument document, Predicate<IMapObject> filter)
-        {
-            var solids = document.Map.Root.FindAll()
-                .Where(x => filter(x))
-                .OfType<Solid>()
-                .Where(x => !x.IsValid())
-                .Select(x => new Problem().Add(x))
-                .ToList();
+		public Task<List<Problem>> Check(MapDocument document, Predicate<IMapObject> filter)
+		{
+			var solids = document.Map.Root.FindAll()
+				.Where(x => filter(x))
+				.OfType<Solid>()
+				.Where(x => !x.IsValid())
+				.Select(x => new Problem().Add(x))
+				.ToList();
 
-            return Task.FromResult(solids);
-        }
+			return Task.FromResult(solids);
+		}
 
-        public Task Fix(MapDocument document, Problem problem)
-        {
-            var delete = new Transaction();
-            foreach (var g in problem.Objects.GroupBy(x => x.Hierarchy.Parent.ID))
-            {
-                delete.Add(new Detatch(g.Key, g));
-            }
-            return MapDocumentOperation.Perform(document, delete);
-        }
-    }
+		public Task Fix(MapDocument document, Problem problem)
+		{
+			var delete = new Transaction();
+			foreach (var g in problem.Objects.GroupBy(x => x.Hierarchy.Parent.ID))
+			{
+				delete.Add(new Detatch(g.Key, g));
+				var newSolid = FixSolid(document, g.First() as Solid);
+				delete.Add(new Attach(g.First().Hierarchy.Parent.ID, newSolid));
+			}
+			return MapDocumentOperation.Perform(document, delete);
+		}
+		private Solid FixSolid(MapDocument document, Solid solid)
+		{
+			var polyhedron = new Polyhedron(solid.GetPolygons().Select(p=>p.Plane));
+			var newSolid = new Solid(document.Map.NumberGenerator.Next("MapObject"));
+
+			newSolid.Data.AddRange(solid.Data.Where(data => data is not Face));
+			foreach (var poly in polyhedron.Polygons)
+			{
+				var oldFace = solid.Faces.FirstOrDefault(x => x.Plane.EquivalentTo(poly.Plane, 0.0075f));
+				var face = new Face(document.Map.NumberGenerator.Next("Face"))
+				{
+					Plane = poly.Plane,
+					Texture = oldFace.Texture,
+				};
+				face.Vertices.AddRange(poly.Vertices);
+				newSolid.Data.Add(face);
+			}
+			newSolid.DescendantsChanged();
+
+			return newSolid;
+		}
+	}
 }
