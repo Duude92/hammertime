3c16718a5e5c4d49da73f408cc91fa29966d2275
Duude92 <switch4acc@gmail.com>
2025-02-17 09:50:10 +0200

    Added class with data for favourites textures

---
+++ Sledge.BspEditor.Tools/Texture/FavouriteTextureFolder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+namespace Sledge.BspEditor.Tools.Texture
+{
+	public class FavouriteTextureFolder
+	{
+		public List<string> Items { get; set; } = new();
+		public List<FavouriteTextureFolder> Children { get; set; } = new();
+		public string Name { get; set; }
+	}
+}
