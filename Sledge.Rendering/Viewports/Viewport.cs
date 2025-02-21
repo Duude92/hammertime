79d7e7facd32a9fb4dd8f3ccebc5e5b64e5d8926
Duude92 <switch4acc@gmail.com>
2025-02-21 13:16:54 +0200

    Updated viewport code. Added resolver to resolve multisampled texture

--- Sledge.Rendering/Viewports/Viewport.cs
+++ Sledge.Rendering/Viewports/Viewport.cs
@@ -3,78 +3,78 @@ using System.Diagnostics;
 using System.Windows.Forms;
 using Sledge.Rendering.Cameras;
 using Sledge.Rendering.Overlay;
+using Sledge.Rendering.Viewports.ViewportResolver;
 using Veldrid;
 
 namespace Sledge.Rendering.Viewports
 {
 	public class Viewport : Control, IViewport
-    {
-        private static int _nextId = 1;
-        private static readonly IntPtr HInstance = Process.GetCurrentProcess().Handle;
+	{
+		private static int _nextId = 1;
+		private static readonly IntPtr HInstance = Process.GetCurrentProcess().Handle;
 
-        private bool _resizeRequired;
+		private bool _resizeRequired;
 
-        public int ID { get; }
-        public Swapchain Swapchain { get; }
+		public int ID { get; }
+		public Swapchain Swapchain { get; }
 
-        public ICamera Camera
-        {
-            get => _camera;
-            set
-            {
-                _camera = value;
-                _camera.Width = Width;
-                _camera.Height = Height;
-            }
-        }
+		public ICamera Camera
+		{
+			get => _camera;
+			set
+			{
+				_camera = value;
+				_camera.Width = Width;
+				_camera.Height = Height;
+			}
+		}
 
-        public Control Control => this;
-
+		public Control Control => this;
 		public Framebuffer ViewportFramebuffer { get; private set; }
 		public ViewportOverlay Overlay { get; }
 		public Resources.Texture ViewportRenderTexture { get; private set; }
-
 		public bool IsFocused => _isFocused;
-
 		public Texture ViewportResolvedTexture { get; private set; }
 
 		private bool _isFocused;
-        private int _unfocusedCounter = 0;
-        private ICamera _camera;
+		private int _unfocusedCounter = 0;
+		private ICamera _camera;
 		private GraphicsDevice _device;
 		private TextureSampleCount _sampleCount;
+		private IViewportResolver _viewportResolver;
+		private Texture _mainSceneColorTexture;
 
 		public event EventHandler<long> OnUpdate;
 
 		public Viewport(GraphicsDevice graphics, GraphicsDeviceOptions options, TextureSampleCount sampleCount)
-        {
+		{
 			_device = graphics;
-            _sampleCount = sampleCount;
-            SetStyle(ControlStyles.Opaque, true);
-            SetStyle(ControlStyles.UserPaint, true);
-            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
-            DoubleBuffered = false;
+			_sampleCount = sampleCount;
+			SetStyle(ControlStyles.Opaque, true);
+			SetStyle(ControlStyles.UserPaint, true);
+			SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+			DoubleBuffered = false;
 
-            var hWnd = Handle; // Will call CreateHandle internally
-            var hInstance = HInstance;
+			var hWnd = Handle; // Will call CreateHandle internally
+			var hInstance = HInstance;
 
-            uint w = (uint)Width, h = (uint)Height;
-            if (w <= 0) w = 1;
-            if (h <= 0) h = 1;
+			uint w = (uint)Width, h = (uint)Height;
+			if (w <= 0) w = 1;
+			if (h <= 0) h = 1;
 
-            ID = _nextId++;
-            Camera = new PerspectiveCamera { Width = Width, Height = Height };
+			ID = _nextId++;
+			Camera = new PerspectiveCamera { Width = Width, Height = Height };
 
-            var source = SwapchainSource.CreateWin32(hWnd, hInstance);
-            var desc = new SwapchainDescription(source, w, h, options.SwapchainDepthFormat, options.SyncToVerticalBlank);
-            Swapchain = graphics.ResourceFactory.CreateSwapchain(desc);
+			var source = SwapchainSource.CreateWin32(hWnd, hInstance);
+			var desc = new SwapchainDescription(source, w, h, options.SwapchainDepthFormat, options.SyncToVerticalBlank);
+			Swapchain = graphics.ResourceFactory.CreateSwapchain(desc);
 
-            InitFramebuffer(w, h, sampleCount);
+			InitFramebuffer(w, h, sampleCount);
 
-            Overlay = new ViewportOverlay(this);
-        }
+			Overlay = new ViewportOverlay(this);
+		}
 		public void InitFramebuffer(TextureSampleCount sampleCount)
-        {
+		{
 			InitFramebuffer((uint)Width, (uint)Height, sampleCount);
 		}
 
@@ -90,7 +90,7 @@ namespace Sledge.Rendering.Viewports
 				PixelFormat.R32_Float,
 				TextureUsage.DepthStencil,
 				 sampleCount));
-			ViewportResolvedTexture = _device.ResourceFactory.CreateTexture(TextureDescription.Texture2D(width, height, 1, 1, PixelFormat.B8_G8_R8_A8_UNorm, TextureUsage.Sampled ,
+			ViewportResolvedTexture = _device.ResourceFactory.CreateTexture(TextureDescription.Texture2D(width, height, 1, 1, PixelFormat.B8_G8_R8_A8_UNorm, TextureUsage.Sampled,
 				TextureSampleCount.Count1));
 			TextureDescription mainColorDesc = TextureDescription.Texture2D(
 				width,
@@ -100,84 +100,92 @@ namespace Sledge.Rendering.Viewports
 				PixelFormat.B8_G8_R8_A8_UNorm,
 				TextureUsage.RenderTarget | TextureUsage.Sampled,
 				sampleCount);
-			var mainSceneColorTexture = _device.ResourceFactory.CreateTexture(ref mainColorDesc);
-			ViewportFramebuffer = _device.ResourceFactory.CreateFramebuffer(new FramebufferDescription(mainSceneDepthTexture, mainSceneColorTexture));
+			_mainSceneColorTexture = _device.ResourceFactory.CreateTexture(ref mainColorDesc);
+			ViewportFramebuffer = _device.ResourceFactory.CreateFramebuffer(new FramebufferDescription(mainSceneDepthTexture, _mainSceneColorTexture));
 			ViewportRenderTexture = new Sledge.Rendering.Resources.Texture(Engine.Engine.Instance.Context, ViewportResolvedTexture, Resources.TextureSampleType.Standard);
+			if (sampleCount == TextureSampleCount.Count1)
+			{
+				_viewportResolver = new SingleSampleResolver();
+			}
+			else
+			{
+				_viewportResolver = new MultisampledResolver();
+			}
 		}
 		protected override bool IsInputKey(Keys keyData)
 		{
 			// Force all keys to be passed to the regular key events
-            return true;
-        }
+			return true;
+		}
 
-        public void Update(long frame)
-        {
-            if (_resizeRequired)
-            {
-                var w = Math.Max(Width, 1);
-                var h = Math.Max(Height, 1);
-                Swapchain.Resize((uint) w, (uint) h);
-                InitFramebuffer((uint)w, (uint)h, _sampleCount);
+		public void Update(long frame)
+		{
+			if (_resizeRequired)
+			{
+				var w = Math.Max(Width, 1);
+				var h = Math.Max(Height, 1);
+				Swapchain.Resize((uint)w, (uint)h);
+				InitFramebuffer((uint)w, (uint)h, _sampleCount);
 				_resizeRequired = false;
-            }
+			}
 
-            OnUpdate?.Invoke(this, frame);
-        }
+			OnUpdate?.Invoke(this, frame);
+		}
 
-        protected override void OnMouseEnter(EventArgs e)
-        {
-            _isFocused = true;
-            base.OnMouseEnter(e);
-        }
+		protected override void OnMouseEnter(EventArgs e)
+		{
+			_isFocused = true;
+			base.OnMouseEnter(e);
+		}
 
-        protected override void OnMouseLeave(EventArgs e)
-        {
-            _isFocused = false;
-            base.OnMouseLeave(e);
-        }
+		protected override void OnMouseLeave(EventArgs e)
+		{
+			_isFocused = false;
+			base.OnMouseLeave(e);
+		}
 
-        protected override void OnResize(EventArgs e)
-        {
-            _resizeRequired = true;
-            Camera.Width = Width;
-            Camera.Height = Height;
-            base.OnResize(e);
-        }
+		protected override void OnResize(EventArgs e)
+		{
+			_resizeRequired = true;
+			Camera.Width = Width;
+			Camera.Height = Height;
+			base.OnResize(e);
+		}
 
-        public bool ShouldRender(long frame)
-        {
-            if (!_isFocused)
-            {
-                _unfocusedCounter++;
+		public bool ShouldRender(long frame)
+		{
+			if (!_isFocused)
+			{
+				_unfocusedCounter++;
 
-                // Update every 10th frame
-                if (_unfocusedCounter % 10 != 0)
-                {
-                    return false;
-                }
-            }
+				// Update every 10th frame
+				if (_unfocusedCounter % 10 != 0)
+				{
+					return false;
+				}
+			}
 
-            _unfocusedCounter = 0;
-            return true;
-        }
+			_unfocusedCounter = 0;
+			return true;
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
 
-            if (disposing)
-            {
-                Overlay.Dispose();
-                Swapchain.Dispose();
+			if (disposing)
+			{
+				Overlay.Dispose();
+				Swapchain.Dispose();
 				ViewportResolvedTexture.Dispose();
-                ViewportFramebuffer.Dispose();
+				ViewportFramebuffer.Dispose();
 				ViewportRenderTexture.Dispose();
 			}
-        }
+		}
 
 		public void ResolveRenderTexture(CommandList commandList)
 		{
-			throw new NotImplementedException();
+			_viewportResolver.ResolveTexture(commandList, _mainSceneColorTexture, ViewportResolvedTexture);
 		}
 	}
 }
