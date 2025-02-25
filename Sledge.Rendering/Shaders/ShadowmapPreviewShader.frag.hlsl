801190ed794b47904ebd129dfb6f45082e1e7614
Duude92 <switch4acc@gmail.com>
2025-02-25 08:06:24 +0200

    Shaders implementation

--- Sledge.Rendering/Shaders/ShadowmapPreviewShader.frag.hlsl
+++ Sledge.Rendering/Shaders/ShadowmapPreviewShader.frag.hlsl
@@ -9,6 +9,8 @@ struct PS_INPUT
 
 float4 main(PS_INPUT input) : SV_Target0
 {
+    return float4(1, 1, 1, 1);
+    
     float2 texcoord = input.TexCoord;
     return Texture.Sample(Sampler, normalize(texcoord));
     
