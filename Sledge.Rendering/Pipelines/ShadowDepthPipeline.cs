fb3d798bc4a7b6e7ad4246de6f9a379daddf8cac
Duude92 <switch4acc@gmail.com>
2025-02-25 08:10:36 +0200

    pipelines implementations

--- Sledge.Rendering/Pipelines/ShadowDepthPipeline.cs
+++ Sledge.Rendering/Pipelines/ShadowDepthPipeline.cs
@@ -1,13 +1,17 @@
-﻿using Sledge.Rendering.Engine;
+﻿using LogicAndTrick.Oy;
+using Sledge.Common.Shell.Context;
+using Sledge.Rendering.Cameras;
+using Sledge.Rendering.Engine;
 using Sledge.Rendering.Primitives;
 using Sledge.Rendering.Renderables;
+using Sledge.Rendering.Resources;
 using Sledge.Rendering.Viewports;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 using Veldrid;
 
 namespace Sledge.Rendering.Pipelines
@@ -25,85 +29,107 @@ namespace Sledge.Rendering.Pipelines
 		private Pipeline _pipeline;
 		private DeviceBuffer _projectionBuffer;
 		private ResourceSet _projectionResourceSet;
+		private ResourceSet _textureSet;
 		public Resources.Texture NearShadowResourceTexture { get; private set; }
-		public Texture NearShadowMap { get; private set; }
+		public Veldrid.Texture NearShadowMap { get; private set; }
 		public TextureView NearShadowMapView { get; private set; }
 		public Framebuffer NearShadowMapFramebuffer { get; private set; }
 
 		private DeviceBuffer _worldAndInverseBuffer;
 		private uint _uniformOffset = 0;
 
+		//private ResourceSet _shadowmapResource;
+		private ResourceSet _shadowWorld;
+		private Bitmap _bitmap;
+		private Veldrid.Texture stagingTexture;
+
 		public void Bind(RenderContext context, CommandList cl, string binding)
 		{
 			//var tex = context.ResourceLoader.GetTexture(binding);
 			//tex?.BindTo(cl, 1);
-			NearShadowResourceTexture?.BindTo(cl,1);
+			//NearShadowResourceTexture?.BindTo(cl,1);
 			cl.SetGraphicsResourceSet(0, _projectionResourceSet);
+			//cl.SetGraphicsResourceSet(1, _shadowWorld);
+			//cl.SetGraphicsResourceSet(2, _textureSet);
 		}
 
 		public void Create(RenderContext context)
 		{
+
 			var gd = context.Device;
 			_uniformOffset = gd.UniformBufferMinOffsetAlignment;
 
 			var factory = gd.ResourceFactory;
-			TextureDescription desc = TextureDescription.Texture2D(2048, 2048, 1, 1, PixelFormat.D32_Float_S8_UInt, TextureUsage.DepthStencil | TextureUsage.Sampled);
-			NearShadowMap = factory.CreateTexture(desc);
+
+			TextureDescription desc = TextureDescription.Texture2D(2048, 2048, 1, 1, PixelFormat.R32_Float, TextureUsage.DepthStencil | TextureUsage.Sampled, TextureSampleCount.Count1);
+			NearShadowMap = factory.CreateTexture(ref desc);
+
+			//TextureDescription desc1 = TextureDescription.Texture2D(2048, 2048, 1, 1, PixelFormat.B8_G8_R8_A8_UNorm, TextureUsage.Sampled | TextureUsage.RenderTarget);
+			//var ColorShadowMap = factory.CreateTexture(desc1);
+
+			//gd.UpdateTexture(NearShadowMap, new byte[2048 * 2048 * 8], 0, 0,0, 2048, 2048, 1, 0, 0);
+
 			NearShadowMap.Name = "Near Shadow Map";
 			NearShadowMapView = factory.CreateTextureView(NearShadowMap);
 			NearShadowMapFramebuffer = factory.CreateFramebuffer(new FramebufferDescription(
-				new FramebufferAttachmentDescription(NearShadowMap, 0), Array.Empty<FramebufferAttachmentDescription>()));
-
-
-			VertexLayoutDescription[] shadowDepthVertexLayouts = new VertexLayoutDescription[]
-			{
-				new VertexLayoutDescription(
-					new VertexElementDescription("Position", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float3),
-					new VertexElementDescription("Normal", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float3),
-					new VertexElementDescription("TexCoord", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float2))
-			};
+				new FramebufferAttachmentDescription(NearShadowMap, 0), Array.Empty<FramebufferAttachmentDescription>() /*new FramebufferAttachmentDescription[] { new FramebufferAttachmentDescription(ColorShadowMap,1) }*/   ));
+			NearShadowMapFramebuffer.Name = "near shadowmap";
 
 			(_vertex, _fragment) = context.ResourceLoader.LoadShaders(Type.ToString());
 
-			ResourceLayout projViewCombinedLayout = context.Device.ResourceFactory.CreateResourceLayout(
-				new ResourceLayoutDescription(
-					new ResourceLayoutElementDescription("Projection", ResourceKind.UniformBuffer, ShaderStages.Vertex)
-				)
+			ResourceLayout projViewCombinedLayout = context.ResourceLoader.ProjectionLayout;
+
+			//ResourceLayout worldLayout = context.Device.ResourceFactory.CreateResourceLayout(new ResourceLayoutDescription(
+			//	new ResourceLayoutElementDescription("WorldAndInverse", ResourceKind.UniformBuffer, ShaderStages.Vertex)));
+
+
+			//VertexLayoutDescription[] shadowDepthVertexLayouts = new VertexLayoutDescription[]
+			//{
+			//	new VertexLayoutDescription(
+			//		new VertexElementDescription("Position", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float3),
+			//		new VertexElementDescription("Normal", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float3),
+			//		new VertexElementDescription("TexCoord", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float2))
+			//};
+
+			var pd = new GraphicsPipelineDescription
+			{
+				BlendState = BlendStateDescription.Empty,
+				DepthStencilState = DepthStencilStateDescription.DepthOnlyLessEqual,
+				RasterizerState = RasterizerStateDescription.Default,
+
+				PrimitiveTopology = PrimitiveTopology.TriangleList,
+				ResourceLayouts = new ResourceLayout[] { projViewCombinedLayout },// ,context.ResourceLoader.TextureLayout/*, worldLayout*/ },
+				ShaderSet = new ShaderSetDescription(new[] { context.ResourceLoader.VertexStandardLayoutDescription }, new[] { _vertex }),
+				Outputs = NearShadowMapFramebuffer.OutputDescription,
+			};
+
+			_pipeline = context.Device.ResourceFactory.CreateGraphicsPipeline(ref pd);
+			_pipeline.Name = "shadowdepthpipeline";
+
+			_projectionBuffer = context.Device.ResourceFactory.CreateBuffer(
+				new BufferDescription((uint)Unsafe.SizeOf<UniformProjection>(), BufferUsage.UniformBuffer)
+			);
+			_projectionBuffer.Name = "projection buffer";
+
+			_projectionResourceSet = context.Device.ResourceFactory.CreateResourceSet(
+				new ResourceSetDescription(context.ResourceLoader.ProjectionLayout, _projectionBuffer)
 			);
 
-			ResourceLayout worldLayout = context.Device.ResourceFactory.CreateResourceLayout(new ResourceLayoutDescription(
-				new ResourceLayoutElementDescription("WorldAndInverse", ResourceKind.UniformBuffer, ShaderStages.Vertex, ResourceLayoutElementOptions.DynamicBinding)));
+
+			_textureSet = gd.ResourceFactory.CreateResourceSet(new ResourceSetDescription(
+				context.ResourceLoader.TextureLayout, NearShadowMapView, context.ResourceLoader.TextureSampler
+			));
+
+			NearShadowResourceTexture = new Resources.Texture(context, NearShadowMap, Resources.TextureSampleType.Standard, _textureSet);
+			NearShadowResourceTexture.GenerateMips = false;
 
 
+			_bitmap = new Bitmap((int)10, (int)10, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+			// Step 1: Create a staging texture
+			stagingTexture = gd.ResourceFactory.CreateTexture(TextureDescription.Texture2D(NearShadowMap.Width, NearShadowMap.Height, 1, 1, NearShadowMap.Format, TextureUsage.Staging));
 
-			_projectionResourceSet = factory.CreateResourceSet(new ResourceSetDescription(
-				worldLayout,
-				new DeviceBufferRange(_worldAndInverseBuffer, _uniformOffset, 128)));
-
-
-
-			GraphicsPipelineDescription depthPD = new GraphicsPipelineDescription(
-				BlendStateDescription.Empty,
-				gd.IsDepthRangeZeroToOne ? DepthStencilStateDescription.DepthOnlyGreaterEqual : DepthStencilStateDescription.DepthOnlyLessEqual,
-				RasterizerStateDescription.Default,
-				PrimitiveTopology.TriangleList,
-				new ShaderSetDescription(
-					shadowDepthVertexLayouts,
-					new[] { _vertex, _fragment },
-					new[] { new SpecializationConstant(100, gd.IsClipSpaceYInverted) }),
-				new ResourceLayout[] { projViewCombinedLayout, worldLayout },
-				new OutputDescription
-				{
-					ColorAttachments = new[] { new OutputAttachmentDescription(PixelFormat.B8_G8_R8_A8_UNorm) },
-					DepthAttachment = new OutputAttachmentDescription(PixelFormat.R32_Float),
-					SampleCount = TextureSampleCount.Count1
-				});
-
-
-			NearShadowResourceTexture = new Resources.Texture(context, NearShadowMap, Resources.TextureSampleType.Standard, _projectionResourceSet);
-
+			Oy.Subscribe("GetDepthImage", () => Oy.Publish("Context:Add", new ContextInfo("DepthImage", _bitmap)));
 		}
-
 		public void Dispose()
 		{
 			_vertex.Dispose();
@@ -115,31 +141,74 @@ namespace Sledge.Rendering.Pipelines
 
 		public void Render(RenderContext context, IViewport target, CommandList cl, IEnumerable<IRenderable> renderables)
 		{
+			if (target.Camera is not PerspectiveCamera) return;
+
+			SetupFrame(context, target);
+			cl.PushDebugGroup("Shadow Map - Near Cascade");
+			cl.SetFramebuffer(NearShadowMapFramebuffer);
+
+			cl.SetFullViewports();
+			cl.ClearDepthStencil(1);
+
 			cl.SetPipeline(_pipeline);
+			//cl.SetGraphicsResourceSet(0, _shadowmapResource);
+
+			//cl.SetGraphicsResourceSet(1, _shadowWorld);
+
 			cl.SetGraphicsResourceSet(0, _projectionResourceSet);
+			//cl.SetGraphicsResourceSet(1, _shadowWorld);
+			//cl.SetGraphicsResourceSet(2, _textureSet);
+			var count = renderables.Count();
 
 			foreach (var r in renderables)
 			{
 				r.Render(context, this, target, cl);
 			}
+
+			cl.PopDebugGroup();
 		}
 
 		public void Render(RenderContext context, IViewport target, CommandList cl, IRenderable renderable, ILocation locationObject)
 		{
 			cl.SetPipeline(_pipeline);
-			cl.SetGraphicsResourceSet(0, _projectionResourceSet);
+			cl.SetGraphicsResourceSet(0, _textureSet);
+			cl.SetGraphicsResourceSet(1, _textureSet);
 
-			renderable.Render(context, this, target, cl, locationObject);
+			renderable.Render(context, this, target, cl, locationObject); 
 		}
-
 		public void SetupFrame(RenderContext context, IViewport target)
 		{
+			if (target.Camera is not PerspectiveCamera) return;
+
+			float orthoSize = 5000f; // Adjust based on scene size
+			float nearPlane = 1f;
+			float farPlane = 5000f;
+
+			Matrix4x4 lightProjection = Matrix4x4.CreateOrthographic(
+				orthoSize, // Width
+				orthoSize, // Height
+				nearPlane,
+				farPlane
+			);
+
+			Vector3 lightPosition = new Vector3(0, 0, 1000); // Light high above the scene
+			Vector3 lightTarget = new Vector3(100, 0, 0); // Looking at the scene center
+			Vector3 upVector = Vector3.UnitZ; // Adjust if needed
+
+
+			Matrix4x4 lightView = Matrix4x4.CreateLookAt(
+	lightPosition,
+	lightTarget,
+	upVector
+);
+
+
 			context.Device.UpdateBuffer(_projectionBuffer, 0, new UniformProjection
 			{
 				Selective = context.SelectiveTransform,
 				Model = Matrix4x4.Identity,
-				View = target.Camera.View,
-				Projection = target.Camera.Projection,
+				View = lightView,
+				Projection = lightProjection
 			});
 		}
 	}
--- Sledge.Rendering/Pipelines/ShadowmapPipeline.cs
+++ Sledge.Rendering/Pipelines/ShadowmapPipeline.cs
@@ -1,73 +1,160 @@
 ﻿using Sledge.Rendering.Engine;
 using Sledge.Rendering.Renderables;
-using Sledge.Rendering.Resources;
 using Sledge.Rendering.Viewports;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Veldrid;
 
 namespace Sledge.Rendering.Pipelines
 {
 	public class ShadowmapPipeline : IPipeline
 	{
-		public PipelineType Type => PipelineType.ShadowMap;
+		public PipelineType Type => PipelineType.Shadowmap;
 
 		public PipelineGroup Group => PipelineGroup.Opaque;
 
-		public float Order => 11;
+		public float Order => 20;
 
 		private Shader _vertex;
 		private Shader _fragment;
 		private Pipeline _pipeline;
+		private ResourceSet _shadowBlendResourceSet;
+		private DeviceBuffer _deviceBuffer;
+		private DeviceBuffer _orthographicBuffer;
 		private DeviceBuffer _projectionBuffer;
 		private ResourceSet _projectionResourceSet;
 
+		private ResourceSet _mainProjViewRS;
+		private Sampler _shadowSampler;
+		private ResourceSet _mainSharedRS;
+		private ResourceSet _mainPerObjectRS;
+		private readonly Func<Resources.Texture> _shadowmapGetter;
+		private readonly Func<TextureView> _viewGetter;
 
-
-
+		public ShadowmapPipeline(Func<Resources.Texture> bindingGetter, Func<TextureView> viewGetter)
+		{
+			_shadowmapGetter = bindingGetter;
+			_viewGetter = viewGetter;
+		}
 		public void Bind(RenderContext context, CommandList cl, string binding)
 		{
-			var tex = context.ResourceLoader.GetTexture(binding);
-			tex?.BindTo(cl, 1);
-			cl.SetGraphicsResourceSet(0, _projectionResourceSet);
+			//var tex = context.ResourceLoader.GetTexture(binding);
+			//tex?.BindTo(cl, 1);
+			//cl.SetGraphicsResourceSet(0, _projectionResourceSet);
 		}
 
 		public void Create(RenderContext context)
 		{
 			var gd = context.Device;
-			var device = gd;
+			var factory = gd.ResourceFactory;
+
 			(_vertex, _fragment) = context.ResourceLoader.LoadShaders(Type.ToString());
 
 
-			VertexLayoutDescription[] mainVertexLayouts = new VertexLayoutDescription[]
+
+			var ResourceLayouts = new ResourceLayout[] { gd.ResourceFactory.CreateResourceLayout(new ResourceLayoutDescription(
+				new ResourceLayoutElementDescription("ShadowMap", ResourceKind.TextureReadOnly, ShaderStages.Fragment),
+				new ResourceLayoutElementDescription("ShadowSampler", ResourceKind.Sampler, ShaderStages.Fragment)
+				))};
+
+			GraphicsPipelineDescription shadowBlendPipelineDesc = new()
 			{
-				new VertexLayoutDescription(
-					new VertexElementDescription("Position", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float3),
-					new VertexElementDescription("Normal", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float3),
-					new VertexElementDescription("TexCoord", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float2))
+				BlendState = BlendStateDescription.SingleDisabled,
+
+				//BlendState = BlendStateDescription.SingleOverrideBlend, // Blend with existing color
+				DepthStencilState = DepthStencilStateDescription.DepthOnlyLessEqual,
+
+				//DepthStencilState = DepthStencilStateDescription.Disabled,
+				RasterizerState = RasterizerStateDescription.Default,
+				PrimitiveTopology = PrimitiveTopology.TriangleList, // For fullscreen quad
+				ResourceLayouts =  ResourceLayouts ,
+				ShaderSet = new ShaderSetDescription(
+		vertexLayouts: Array.Empty<VertexLayoutDescription>(), // No vertex input needed
+					shaders: new[] { _vertex, _fragment }),
+				Outputs = new OutputDescription
+				{
+					ColorAttachments = new[] { new OutputAttachmentDescription(PixelFormat.B8_G8_R8_A8_UNorm) },
+					DepthAttachment = new OutputAttachmentDescription(PixelFormat.R32_Float),
+					SampleCount = TextureSampleCount.Count1
+				},
 			};
 
+			_pipeline = factory.CreateGraphicsPipeline(ref shadowBlendPipelineDesc);
 
 
-			//GraphicsPipelineDescription mainPD = new GraphicsPipelineDescription(
-			//	BlendStateDescription.SingleOverrideBlend,
-			//	gd.IsDepthRangeZeroToOne ? DepthStencilStateDescription.DepthOnlyGreaterEqual : DepthStencilStateDescription.DepthOnlyLessEqual,
-			//	RasterizerStateDescription.Default,
-			//	PrimitiveTopology.TriangleList,
-			//	new ShaderSetDescription(mainVertexLayouts, new[] { _vertex, _fragment }, new[] { new SpecializationConstant(100, gd.IsClipSpaceYInverted) }),
-			//	new ResourceLayout[] { projViewLayout, mainSharedLayout, mainPerObjectLayout, reflectionLayout },
-			//	new OutputDescription
-			//	{
-			//		ColorAttachments = new[] { new OutputAttachmentDescription(PixelFormat.B8_G8_R8_A8_UNorm) },
-			//		DepthAttachment = new OutputAttachmentDescription(PixelFormat.R32_Float),
-			//		SampleCount = TextureSampleCount.Count1
-			//	});
-			//var sc = context.ResourceLoader.ProjectionLayout;
+			_shadowBlendResourceSet = factory.CreateResourceSet(new ResourceSetDescription(
+				ResourceLayouts[0],
+				_viewGetter(), // This should be your shadow depth texture
+				gd.PointSampler // A sampler, like linear or nearest filtering
+			));
+
+	//		_deviceBuffer = factory.CreateBuffer(new BufferDescription(
+	//3 * 2 * sizeof(float), // 3 vertices, 2 floats (XY) per vertex
+	//BufferUsage.VertexBuffer
+	//		));
 
 
+
+
+			//			var applyShadowPipelineDesc = new GraphicsPipelineDescription
+			//			{
+			//				BlendState = BlendStateDescription.SingleOverrideBlend,
+			//				DepthStencilState = DepthStencilStateDescription.Disabled,
+			//				RasterizerState = RasterizerStateDescription.Default,
+			//				PrimitiveTopology = PrimitiveTopology.TriangleList,
+			//				ResourceLayouts = new ResourceLayout[] { context.ResourceLoader.ProjectionLayout, context.ResourceLoader.TextureLayout },
+			//				ShaderSet = new ShaderSetDescription(
+			//		vertexLayouts: new VertexLayoutDescription[] { context.ResourceLoader.VertexStandardLayoutDescription },
+			//		shaders: new[] { _vertex, _fragment }),
+			//				Outputs = new OutputDescription
+			//				{
+			//					ColorAttachments = new[] { new OutputAttachmentDescription(PixelFormat.B8_G8_R8_A8_UNorm) },
+			//					DepthAttachment = new OutputAttachmentDescription(PixelFormat.R32_Float),
+			//					SampleCount = TextureSampleCount.Count1
+			//				},
+
+			//				ResourceBindingModel = ResourceBindingModel.Improved
+			//			};
+			//			_pipeline = factory.CreateGraphicsPipeline(ref applyShadowPipelineDesc);
+
+			//			_orthographicBuffer = factory.CreateBuffer(new BufferDescription((uint)Unsafe.SizeOf<UniformProjection>(), BufferUsage.UniformBuffer));
+
+
+
+			//			_mainProjViewRS = gd.ResourceFactory.CreateResourceSet(new ResourceSetDescription(
+			//				context.ResourceLoader.ProjectionLayout,
+			//				_orthographicBuffer
+			//				/*_viewGetter(),
+			//				gd.PointSampler*/));
+
+			//			var sampler = context.ResourceLoader.TextureSampler;
+
+			//			_mainPerObjectRS = gd.ResourceFactory.CreateResourceSet(new ResourceSetDescription(
+			//	context.ResourceLoader.TextureLayout, _viewGetter(), sampler
+			//));
+
+			//			_shadowSampler = factory.CreateSampler(new SamplerDescription(
+			//				SamplerAddressMode.Clamp,
+			//				SamplerAddressMode.Clamp,
+			//				SamplerAddressMode.Clamp,
+			//				/*SamplerFilter.ComparisonMinLinear_MagLinear_MipLinear*/
+			//				SamplerFilter.MinLinear_MagLinear_MipLinear,
+			//				ComparisonKind.LessEqual,
+			//				0,
+			//				0,
+			//				0,
+			//				0,
+			//				SamplerBorderColor.OpaqueBlack));
+
+
+			//			_projectionBuffer = context.Device.ResourceFactory.CreateBuffer(
+			//	new BufferDescription((uint)Unsafe.SizeOf<UniformProjection>(), BufferUsage.UniformBuffer)
+			//);
+			//			_projectionBuffer.Name = "projection buffer";
+
+			//			_projectionResourceSet = context.Device.ResourceFactory.CreateResourceSet(
+			//				new ResourceSetDescription(context.ResourceLoader.ProjectionLayout, _projectionBuffer)
+			//			);
 		}
 
 		public void Dispose()
@@ -77,17 +164,48 @@ namespace Sledge.Rendering.Pipelines
 
 		public void Render(RenderContext context, IViewport target, CommandList cl, IEnumerable<IRenderable> renderables)
 		{
-			throw new NotImplementedException();
+
+			cl.SetPipeline(_pipeline);
+
+			//if (target.Camera is OrthographicCamera) return;
+			cl.PushDebugGroup("Shadow Map - Apply shadow");
+
+			cl.SetGraphicsResourceSet(0, _shadowBlendResourceSet);
+			_shadowmapGetter().BindTo(cl, 0);
+			//cl.SetGraphicsResourceSet(1, _mainPerObjectRS);
+			//var count = renderables.Count();
+			//foreach (var r in renderables)
+			//{
+
+			//	r.Render(context, this, target, cl);
+			//}
+			cl.Draw(3, 1, 0, 0); // Draw a full-screen triangle with 3 vertices
+
+			cl.PopDebugGroup();
 		}
 
 		public void Render(RenderContext context, IViewport target, CommandList cl, IRenderable renderable, ILocation locationObject)
 		{
+			return;
 			throw new NotImplementedException();
 		}
 
 		public void SetupFrame(RenderContext context, IViewport target)
 		{
-			throw new NotImplementedException();
+			//context.Device.UpdateBuffer(_deviceBuffer, 0, new float[]
+			//{
+			//	-1f, -1f, // Bottom-left
+			//	3f, -1f, // Bottom-right (oversized for fullscreen triangle trick)
+			//	-1f,  3f  // Top-left (oversized)
+			//});
+
+			//context.Device.UpdateBuffer(_projectionBuffer, 0, new UniformProjection
+			//{
+			//	Selective = context.SelectiveTransform,
+			//	Model = Matrix4x4.Identity,
+			//	View = target.Camera.View,
+			//	Projection = target.Camera.Projection,
+			//});
 		}
 	}
 }
