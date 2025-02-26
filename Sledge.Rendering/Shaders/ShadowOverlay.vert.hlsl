f8c97de7f788cd646397b58d1d042e0799ed6ac6
Duude92 <switch4acc@gmail.com>
2025-02-26 15:24:08 +0200

    Added shadow overlay shaders

---
+++ Sledge.Rendering/Shaders/ShadowOverlay.vert.hlsl
@@ -0,0 +1,73 @@
+struct VertexIn
+{
+    float3 Position : POSITION0;
+    float3 Normal : NORMAL0;
+    float4 Colour : COLOR0;
+    float2 Texture : TEXCOORD0;
+    float4 Tint : COLOR1;
+    uint1 Flags : POSITION1;
+};
+
+struct FragmentIn
+{
+    float4 fPosition : SV_Position;
+    float4 fNormal : NORMAL0;
+    float4 fColour : COLOR0;
+    float2 fTexture : TEXCOORD0;
+    float4 fTint : COLOR1;
+    uint1 fFlags : POSITION1;
+    float4 sPosition : TEXCOORD1;
+};
+
+cbuffer Projection
+{
+    matrix Selective;
+    matrix Model;
+    matrix View;
+    matrix Projection;
+};
+cbuffer LightData
+{
+    matrix LightView;
+};
+cbuffer LightData2
+{
+    matrix LightProjection;
+};
+static const uint Flags_SelectiveTransformed = 1 << 0;
+
+static const float4x4 Identity = { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
+
+
+
+FragmentIn main(VertexIn input)
+{
+    matrix tModel = transpose(Model);
+    matrix tView = transpose(View);
+    matrix tProjection = transpose(Projection);
+
+    FragmentIn output;
+    
+    float4 position = float4(input.Position, 1);
+    float4 normal = float4(input.Normal, 1);
+    
+    position = mul(position, lerp(Identity, transpose(Selective), (input.Flags.x & Flags_SelectiveTransformed) / Flags_SelectiveTransformed));
+    
+    float4 modelPos = mul(position, tModel);
+    float4 cameraPos = mul(modelPos, tView);
+    float4 viewportPos = mul(cameraPos, tProjection);
+    
+    tView = transpose(LightView);
+    tProjection = transpose(LightProjection);
+
+
+    output.fPosition = viewportPos;
+    output.fNormal = normal;
+    output.fColour = input.Colour;
+    output.fTexture = input.Texture;
+    output.fTint = input.Tint;
+    output.fFlags = input.Flags;
+    output.sPosition = mul(mul(mul(float4(input.Position, 1.0), tModel), tView), tProjection);
+
+    return output;
+}
