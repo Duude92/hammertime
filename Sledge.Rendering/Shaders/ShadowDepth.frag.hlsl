cde76e00674734091832c07c2621ff4190268fce
Duude92 <switch4acc@gmail.com>
2025-02-27 18:02:27 +0200

    Fix fragment shader with depth value

--- Sledge.Rendering/Shaders/ShadowDepth.frag.hlsl
+++ Sledge.Rendering/Shaders/ShadowDepth.frag.hlsl
@@ -1,13 +1,10 @@
-Texture2D Texture;
-SamplerState Sampler;
-
 struct PS_INPUT
 {
     float4 Position : SV_Position;
-    float2 TexCoord : TEXCOORD0;
 };
 
 float4 main(PS_INPUT input) : SV_Target0
 {
-    return Texture.Sample(Sampler, normalize(input.TexCoord));
+    float depthValue = input.Position.z / input.Position.w;
+    return float4(depthValue, depthValue, depthValue, 1.0f);
 }
