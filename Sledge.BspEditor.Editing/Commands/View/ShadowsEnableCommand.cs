7d1ca3c835a6580367e77cc07104218390e29a8d
Duude92 <switch4acc@gmail.com>
2025-03-05 11:43:57 +0200

    Added command + icon for command

---
+++ Sledge.BspEditor.Editing/Commands/View/ShadowsEnableCommand.cs
@@ -0,0 +1,38 @@
+﻿using LogicAndTrick.Oy;
+using Sledge.BspEditor.Commands;
+using Sledge.BspEditor.Documents;
+using Sledge.BspEditor.Editing.Properties;
+using Sledge.Common.Shell.Commands;
+using Sledge.Common.Shell.Context;
+using Sledge.Common.Shell.Menu;
+using Sledge.Common.Translations;
+using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+namespace Sledge.BspEditor.Editing.Commands.View
+{
+	[AutoTranslate]
+	[Export(typeof(ICommand))]
+	[CommandID("BspEditor:View:Shadows")]
+	[MenuItem("View", "", "Rendering", "Y")]
+	[MenuImage(typeof(Resources), nameof(Resources.Menu_Shadows))]
+
+	public class ShadowsEnableCommand : BaseCommand, IMenuItemExtendedProperties
+	{
+		public override string Name { get; set; } = "Enable shadows";
+		public override string Details { get; set; } = "Enable shadows rendering";
+
+		public bool IsToggle => true;
+
+		protected override async Task Invoke(MapDocument document, CommandParameters parameters)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool GetToggleState(IContext context)
+		{
+			return IsToggle;
+		}
+	}
+}
--- Sledge.BspEditor.Editing/Properties/Resources.Designer.cs
+++ Sledge.BspEditor.Editing/Properties/Resources.Designer.cs
@@ -303,6 +303,16 @@ namespace Sledge.BspEditor.Editing.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Menu_Shadows {
+            get {
+                object obj = ResourceManager.GetObject("Menu_Shadows", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Menu_ShowBrushID {
             get {
                 object obj = ResourceManager.GetObject("Menu_ShowBrushID", resourceCulture);
