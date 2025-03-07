30ff6ecae5bd32e2b79dda271c82041daf4a0d43
Duude92 <switch4acc@gmail.com>
2025-03-07 15:41:29 +0200

    Filter solids with requesting parent entity

--- Sledge.BspEditor/Commands/Grouping/Group.cs
+++ Sledge.BspEditor/Commands/Grouping/Group.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Sledge.BspEditor.Documents;
 using Sledge.BspEditor.Modification;
 using Sledge.BspEditor.Modification.Operations.Tree;
+using Sledge.BspEditor.Primitives.MapObjects;
 using Sledge.BspEditor.Properties;
 using Sledge.Common.Shell.Commands;
 using Sledge.Common.Shell.Hotkeys;
@@ -12,34 +13,35 @@ using Sledge.Common.Translations;
 
 namespace Sledge.BspEditor.Commands.Grouping
 {
-    [AutoTranslate]
-    [Export(typeof(ICommand))]
-    [CommandID("BspEditor:Edit:Group")]
-    [DefaultHotkey("Ctrl+G")]
-    [MenuItem("Tools", "", "Group", "D")]
-    [MenuImage(typeof(Resources), nameof(Resources.Menu_Group))]
-    public class Group : BaseCommand
-    {
-        public override string Name { get; set; } = "Group";
-        public override string Details { get; set; } = "Group selected objects";
+	[AutoTranslate]
+	[Export(typeof(ICommand))]
+	[CommandID("BspEditor:Edit:Group")]
+	[DefaultHotkey("Ctrl+G")]
+	[MenuItem("Tools", "", "Group", "D")]
+	[MenuImage(typeof(Resources), nameof(Resources.Menu_Group))]
+	public class Group : BaseCommand
+	{
+		public override string Name { get; set; } = "Group";
+		public override string Details { get; set; } = "Group selected objects";
 
-        protected override async Task Invoke(MapDocument document, CommandParameters parameters)
-        {
-            var sel = document.Selection.GetSelectedParents().ToList();
-            if (sel.Count > 1)
-            {
-                var group = new Primitives.MapObjects.Group(document.Map.NumberGenerator.Next("MapObject")) { IsSelected = true };
+		protected override async Task Invoke(MapDocument document, CommandParameters parameters)
+		{
+			var sel = document.Selection.GetSelectedParents().Select(@object => @object.Hierarchy.Parent != document.Map.Root && @object.Hierarchy.Parent is Entity entity ? entity : @object).ToList();
 
-                var tns = new Transaction();
-                foreach (var grp in sel.GroupBy(x => x.Hierarchy.Parent.ID))
-                {
-                    tns.Add(new Detatch(grp.Key, grp));
-                }
-                tns.Add(new Attach(document.Map.Root.ID, group));
-                tns.Add(new Attach(group.ID, sel));
+			if (sel.Count > 1)
+			{
+				var group = new Primitives.MapObjects.Group(document.Map.NumberGenerator.Next("MapObject")) { IsSelected = true };
 
-                await MapDocumentOperation.Perform(document, tns);
-            }
-        }
-    }
+				var tns = new Transaction();
+				foreach (var grp in sel.GroupBy(x => x.Hierarchy.Parent.ID))
+				{
+					tns.Add(new Detatch(grp.Key, grp));
+				}
+				tns.Add(new Attach(document.Map.Root.ID, group));
+				tns.Add(new Attach(group.ID, sel));
+
+				await MapDocumentOperation.Perform(document, tns);
+			}
+		}
+	}
 }
