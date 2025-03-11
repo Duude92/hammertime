5a9331b0bca8da122fcd99729a3e4aa8741591ef
Duude92 <switch4acc@gmail.com>
2025-03-11 08:51:01 +0200

    Unlink MdlModel from ModelRenderable to be able to render each entity with same model separately

--- Sledge.BspEditor.Rendering/Resources/ResourceCollection.cs
+++ Sledge.BspEditor.Rendering/Resources/ResourceCollection.cs
@@ -56,12 +56,14 @@ namespace Sledge.BspEditor.Rendering.Resources
             EnsureEnvironment(environment);
             var mlist = _models[environment.ID];
             var rlist = _resources[environment.ID];
-
+            
+            /// Since MdlModel class also provides code for rendering - we have duplicating renderers, that would affect each other same model file
+            /*
             // Check if the model has already been loaded
             var existing = mlist.FirstOrDefault(x =>
                 string.Equals(x.Name, path, StringComparison.InvariantCultureIgnoreCase));
             if (existing != null) return existing.Model;
-
+            */
             // Find the file
             var file = environment.Root.TraversePath(path);
             if (file == null || !file.Exists) return null;
