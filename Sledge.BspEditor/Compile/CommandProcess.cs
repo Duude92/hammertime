0254b607fe20e5dbd13511134a977ca6030304b7
Duude92 <switch4acc@gmail.com>
2025-08-19 10:32:18 +0300

    Add CommandProcess, that allows to run command without checking it existance

---
+++ Sledge.BspEditor/Compile/CommandProcess.cs
@@ -0,0 +1,20 @@
+﻿using Sledge.BspEditor.Documents;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Sledge.BspEditor.Compile
+{
+	internal class CommandProcess : BatchProcess
+	{
+		public CommandProcess(BatchStepType stepType, string process) : base(stepType, RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "cmd" : "", $"/c {process}")
+		{
+		}
+
+		public override async Task Run(Batch batch, MapDocument document)
+		{
+			await RunInternal(batch, document);
+		}
+
+	}
+}
