5ad20b1e8934097aedfa97a59422bb783e28e03e
Duude92 <switch4acc@gmail.com>
2025-08-31 16:47:05 +0300

    Fix entrypoint to expose BuildAvaloniaApp for avalonia designer

--- Sledge.Editor/Program.cs
+++ Sledge.Editor/Program.cs
@@ -6,25 +6,25 @@ using Avalonia;
 
 namespace Sledge.Editor
 {
-    static class Program
-    {
-        [STAThread]
-        static void Main()
-        {
-            // From: https://stackoverflow.com/a/9160150
-            // This is the easiest and most consistent way to force decimals to be used in winforms controls.
-            // Users expect decimals to be displayed, since that's how VHE and other editors behave.
-            // This is a deviation from expected behaviour of an application, which is why this is in
-            // the editor bootstrapper, and not in the shell.
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			// From: https://stackoverflow.com/a/9160150
+			// This is the easiest and most consistent way to force decimals to be used in winforms controls.
+			// Users expect decimals to be displayed, since that's how VHE and other editors behave.
+			// This is a deviation from expected behaviour of an application, which is why this is in
+			// the editor bootstrapper, and not in the shell.
 
-            var forceDecimalCulture = (CultureInfo)Thread.CurrentThread.CurrentCulture.Clone();
+			var forceDecimalCulture = (CultureInfo)Thread.CurrentThread.CurrentCulture.Clone();
 
-            forceDecimalCulture.NumberFormat.NumberDecimalSeparator = ".";
-            forceDecimalCulture.NumberFormat.NumberGroupSeparator = ",";
-            forceDecimalCulture.NumberFormat.NumberGroupSizes = new [] { 3 };
-            forceDecimalCulture.NumberFormat.NumberNegativePattern = 1;
+			forceDecimalCulture.NumberFormat.NumberDecimalSeparator = ".";
+			forceDecimalCulture.NumberFormat.NumberGroupSeparator = ",";
+			forceDecimalCulture.NumberFormat.NumberGroupSizes = new[] { 3 };
+			forceDecimalCulture.NumberFormat.NumberNegativePattern = 1;
 
-            Thread.CurrentThread.CurrentCulture = forceDecimalCulture;
+			Thread.CurrentThread.CurrentCulture = forceDecimalCulture;
 
 			AppBuilder.Configure<SingleInstanceAv>()
 	 .UseWin32()
@@ -33,6 +33,12 @@ namespace Sledge.Editor
 
 			// We're finished ruining the culture, let's run the app now.
 			Startup.Run();
-        }
-    }
+		}
+		public static AppBuilder BuildAvaloniaApp()
+		{
+			return AppBuilder.Configure<SingleInstanceAv>()
+			.UsePlatformDetect()
+			.LogToTrace();
+		}
+	}
 }
