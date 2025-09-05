a8dee1eb5f382a91a7d0a606a7ff48c45bdb11c2
Duude92 <switch4acc@gmail.com>
2025-09-05 09:16:32 +0300

    Update StatusRegister for avalonia

--- Sledge.Shell/Registers/StatusRegister.cs
+++ Sledge.Shell/Registers/StatusRegister.cs
@@ -3,94 +3,105 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Sledge.Common.Shell.Hooks;
 using System.ComponentModel.Composition;
-using System.Drawing;
 using System.Linq;
-using System.Windows.Forms;
 using LogicAndTrick.Oy;
 using Sledge.Common.Shell.Components;
 using Sledge.Common.Shell.Context;
+using Avalonia.Controls;
+using Avalonia.Threading;
+using Avalonia.Media;
 
 namespace Sledge.Shell.Registers
 {
-    [Export(typeof(IInitialiseHook))]
-    public class StatusRegister : IInitialiseHook
-    {
-        // The menu register needs direct access to the shell
-        [Import] private Forms.Shell _shell;
+	[Export(typeof(IInitialiseHook))]
+	public class StatusRegister : IInitialiseHook
+	{
+		// The menu register needs direct access to the shell
+		[Import] private Forms.ShellAv _shell;
 
-        [ImportMany] private IEnumerable<Lazy<IStatusItem>> _statusItems;
-        
-        public Task OnInitialise()
-        {
-            foreach (var si in _statusItems.OrderBy(x => OrderHintAttribute.GetOrderHint(x.Value.GetType())))
-            {
-                Add(si.Value);
-            }
+		[ImportMany] private IEnumerable<Lazy<IStatusItem>> _statusItems;
 
-            // Subscribe to context changes
-            Oy.Subscribe<IContext>("Context:Changed", ContextChanged);
+		public Task OnInitialise()
+		{
+			foreach (var si in _statusItems.OrderBy(x => OrderHintAttribute.GetOrderHint(x.Value.GetType())))
+			{
+				Add(si.Value);
+			}
 
-            return Task.FromResult(0);
-        }
-        
-        private List<StatusBarItem> _items;
+			// Subscribe to context changes
+			Oy.Subscribe<IContext>("Context:Changed", ContextChanged);
 
-        public StatusRegister()
-        {
-            _items = new List<StatusBarItem>();
-        }
+			return Task.FromResult(0);
+		}
 
-        public void Add(IStatusItem item)
-        {
-            var si = new StatusBarItem(item);
-            _items.Add(si);
-            _shell.InvokeLater(() => _shell.StatusStrip.Items.Add(si.Label));
-        }
+		private List<StatusBarItem> _items;
 
-        private Task ContextChanged(IContext context)
-        {
-            foreach (var si in _items)
-            {
-                si.ContextChanged(context);
-            }
-            return Task.FromResult(0);
-        }
+		public StatusRegister()
+		{
+			_items = new List<StatusBarItem>();
+		}
 
-        private class StatusBarItem
-        {
-            public IStatusItem Item { get; set; }
-            public ToolStripStatusLabel Label { get; set; }
+		public void Add(IStatusItem item)
+		{
+			var si = new StatusBarItem(item);
+			_items.Add(si);
+			_shell.InvokeLater(() => _shell.StatusStrip.Children.Add(si.Label));
+		}
 
-            public StatusBarItem(IStatusItem item)
-            {
-                Item = item;
-                item.TextChanged += TextChanged;
-                Label = new ToolStripStatusLabel
-                {
-                    Text = item.Text ?? "",
-                    BorderSides = item.HasBorder ? ToolStripStatusLabelBorderSides.All : ToolStripStatusLabelBorderSides.None,
-                    AutoSize = item.Width <= 0,
-                    Spring = item.Width <= 0,
-                    TextAlign = item.Width <= 0 ? ContentAlignment.MiddleLeft : ContentAlignment.MiddleCenter,
-                    Width = Math.Max(1, item.Width)
-                };
-            }
+		private Task ContextChanged(IContext context)
+		{
+			foreach (var si in _items)
+			{
+				si.ContextChanged(context);
+			}
+			return Task.FromResult(0);
+		}
 
-            private void TextChanged(object sender, string text)
-            {
-                Label.Owner.InvokeLater(() =>
-                {
-                    Label.Text = text;
-                });
-            }
+		private class StatusBarItem
+		{
+			public IStatusItem Item { get; set; }
+			public Label Label { get; set; }
 
-            public void ContextChanged(IContext context)
-            {
-                Label.Owner.InvokeLater(() =>
-                {
-                    Label.Visible = Item.IsInContext(context);
-                });
-            }
-        }
-    }
+			public StatusBarItem(IStatusItem item)
+			{
+				Item = item;
+				item.TextChanged += TextChanged;
+				Dispatcher.UIThread.Post(() =>
+				Label = new Label
+				{
+					Content = item.Text ?? "",
+
+					Width = item.Width > 0 ? Math.Max(1, item.Width) : double.NaN,
+					HorizontalContentAlignment = Avalonia.Layout.HorizontalAlignment.Left,
+					BorderThickness = item.HasBorder ? new Avalonia.Thickness(1) : new Avalonia.Thickness(0),
+					BorderBrush = Brushes.Black
+				});
+				//Label = new ToolStripStatusLabel
+				//{
+				//	Text = item.Text ?? "",
+				//	BorderSides = item.HasBorder ? ToolStripStatusLabelBorderSides.All : ToolStripStatusLabelBorderSides.None,
+				//	AutoSize = item.Width <= 0,
+				//	Spring = item.Width <= 0,
+				//	TextAlign = item.Width <= 0 ? ContentAlignment.MiddleLeft : ContentAlignment.MiddleCenter,
+				//	Width = Math.Max(1, item.Width)
+				//};
+			}
+
+			private void TextChanged(object sender, string text)
+			{
+				Label.InvokeLater(() =>
+				{
+					Label.Content = text;
+				});
+			}
+
+			public void ContextChanged(IContext context)
+			{
+				Label.InvokeLater(() =>
+				{
+					Label.IsVisible = Item.IsInContext(context);
+				});
+			}
+		}
+	}
 }
