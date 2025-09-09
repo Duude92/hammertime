b8f312746a6404388771ef22457a18b2f02c6be5
Duude92 <switch4acc@gmail.com>
2025-09-09 12:19:07 +0300

    Fix detaching solids on creating new entity when solid is part of entity Fix reversing detach solids

--- Sledge.BspEditor/Modification/Operations/Tree/Detatch.cs
+++ Sledge.BspEditor/Modification/Operations/Tree/Detatch.cs
@@ -6,67 +6,68 @@ using Sledge.BspEditor.Primitives.MapObjects;
 
 namespace Sledge.BspEditor.Modification.Operations.Tree
 {
-    public class Detatch : IOperation
-    {
-        private long _parentId;
-        private List<long> _idsToDetatch;
-        private List<IMapObject> _detatchedObjects;
-        public bool Trivial => false;
+	public class Detatch : IOperation
+	{
+		private long _parentId;
+		private List<long> _idsToDetatch;
+		private List<IMapObject> _detatchedObjects;
+		public bool Trivial => false;
 
-        public Detatch(long parentId, params IMapObject[] objectsToDetatch)
-        {
-            _parentId = parentId;
-            _idsToDetatch = objectsToDetatch.Select(x => x.ID).ToList();
-        }
+		public Detatch(long parentId, params IMapObject[] objectsToDetatch)
+		{
+			_parentId = parentId;
+			_idsToDetatch = objectsToDetatch.Select(x => x.ID).ToList();
+		}
 
-        public Detatch(long parentId, IEnumerable<IMapObject> objectsToDetatch)
-        {
-            _parentId = parentId;
-            _idsToDetatch = objectsToDetatch.Select(x => x.ID).ToList();
-        }
+		public Detatch(long parentId, IEnumerable<IMapObject> objectsToDetatch)
+		{
+			_parentId = parentId;
+			_idsToDetatch = objectsToDetatch.Select(x => x.ID).ToList();
+		}
 
-        public async Task<Change> Perform(MapDocument document)
-        {
-            var ch = new Change(document);
+		public async Task<Change> Perform(MapDocument document)
+		{
+			var ch = new Change(document);
 
-            var par = document.Map.Root.FindByID(_parentId);
-            _detatchedObjects = _idsToDetatch.Select(x => par.FindByID(x)).Where(x => x != null).ToList();
+			var par = document.Map.Root.FindByID(_parentId);
+			if (par == null) return ch;
+			_detatchedObjects = _idsToDetatch.Select(x => par.FindByID(x)).Where(x => x != null).ToList();
 
-            foreach (var o in _detatchedObjects)
-            {
-                // Remove parent
-                ch.Remove(o);
+			foreach (var o in _detatchedObjects)
+			{
+				// Remove parent
+				ch.Remove(o);
 
-                // Remove all descendants
-                ch.RemoveRange(o.FindAll());
+				// Remove all descendants
+				ch.RemoveRange(o.FindAll());
 
-                o.Hierarchy.Parent = null;
-            }
-            _idsToDetatch = null;
+				o.Hierarchy.Parent = null;
+			}
+			_idsToDetatch = null;
 
-            return ch;
-        }
+			return ch;
+		}
 
-        public async Task<Change> Reverse(MapDocument document)
-        {
-            var ch = new Change(document);
+		public async Task<Change> Reverse(MapDocument document)
+		{
+			var ch = new Change(document);
 
-            var par = document.Map.Root.FindByID(_parentId);
-            _idsToDetatch = _detatchedObjects.Select(x => x.ID).ToList();
+			var par = document.Map.Root.FindByID(_parentId);
+			_detatchedObjects = _detatchedObjects ?? new List<IMapObject>();
+			_idsToDetatch = _detatchedObjects?.Select(x => x.ID).ToList();
+			foreach (var o in _detatchedObjects)
+			{
+				// Add parent
+				ch.Add(o);
 
-            foreach (var o in _detatchedObjects)
-            {
-                // Add parent
-                ch.Add(o);
+				// Add all descendants
+				ch.AddRange(o.FindAll());
 
-                // Add all descendants
-                ch.AddRange(o.FindAll());
-                
-                o.Hierarchy.Parent = par;
-            }
-            _detatchedObjects = null;
+				o.Hierarchy.Parent = par;
+			}
+			_detatchedObjects = null;
 
-            return ch;
-        }
-    }
+			return ch;
+		}
+	}
 }
