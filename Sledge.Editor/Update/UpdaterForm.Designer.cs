584a0767f47ae9a4dc20dd0d8a808828b88bf630
Duude92 <switch4acc@gmail.com>
2025-11-14 16:08:50 +0200

    Add option control to select arch

--- Sledge.Editor/Update/UpdaterForm.Designer.cs
+++ Sledge.Editor/Update/UpdaterForm.Designer.cs
@@ -20,122 +20,152 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.StatusLabel = new System.Windows.Forms.Label();
-            this.ProgressBar = new System.Windows.Forms.ProgressBar();
-            this.CancelButton = new System.Windows.Forms.Button();
-            this.StartButton = new System.Windows.Forms.Button();
-            this.ReleaseDetails = new System.Windows.Forms.TextBox();
-            this.ReleaseNotesLink = new System.Windows.Forms.LinkLabel();
-            this.SuspendLayout();
-            // 
-            // StatusLabel
-            // 
-            this.StatusLabel.AutoSize = true;
-            this.StatusLabel.Location = new System.Drawing.Point(12, 9);
-            this.StatusLabel.Name = "StatusLabel";
-            this.StatusLabel.Size = new System.Drawing.Size(66, 13);
-            this.StatusLabel.TabIndex = 0;
-            this.StatusLabel.Text = "Status Label";
-            // 
-            // ProgressBar
-            // 
-            this.ProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.ProgressBar.Location = new System.Drawing.Point(12, 246);
-            this.ProgressBar.Name = "ProgressBar";
-            this.ProgressBar.Size = new System.Drawing.Size(293, 10);
-            this.ProgressBar.Step = 1;
-            this.ProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
-            this.ProgressBar.TabIndex = 1;
-            // 
-            // CancelButton
-            // 
-            this.CancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.CancelButton.Location = new System.Drawing.Point(311, 233);
-            this.CancelButton.Name = "CancelButton";
-            this.CancelButton.Size = new System.Drawing.Size(75, 23);
-            this.CancelButton.TabIndex = 2;
-            this.CancelButton.Text = "Cancel";
-            this.CancelButton.UseVisualStyleBackColor = true;
-            this.CancelButton.Click += new System.EventHandler(this.CancelButtonClicked);
-            // 
-            // StartButton
-            // 
-            this.StartButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.StartButton.Location = new System.Drawing.Point(311, 12);
-            this.StartButton.Name = "StartButton";
-            this.StartButton.Size = new System.Drawing.Size(75, 23);
-            this.StartButton.TabIndex = 2;
-            this.StartButton.Text = "Download";
-            this.StartButton.UseVisualStyleBackColor = true;
-            this.StartButton.Click += new System.EventHandler(this.DownloadButtonClicked);
-            // 
-            // ReleaseDetails
-            // 
-            this.ReleaseDetails.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.ReleaseDetails.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ReleaseDetails.Location = new System.Drawing.Point(15, 41);
-            this.ReleaseDetails.Multiline = true;
-            this.ReleaseDetails.Name = "ReleaseDetails";
-            this.ReleaseDetails.ReadOnly = true;
-            this.ReleaseDetails.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.ReleaseDetails.Size = new System.Drawing.Size(371, 170);
-            this.ReleaseDetails.TabIndex = 3;
-            // 
-            // ReleaseNotesLink
-            // 
-            this.ReleaseNotesLink.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.ReleaseNotesLink.AutoSize = true;
-            this.ReleaseNotesLink.Location = new System.Drawing.Point(12, 218);
-            this.ReleaseNotesLink.Name = "ReleaseNotesLink";
-            this.ReleaseNotesLink.Size = new System.Drawing.Size(252, 13);
-            this.ReleaseNotesLink.TabIndex = 7;
-            this.ReleaseNotesLink.TabStop = true;
-            this.ReleaseNotesLink.Text = "Click here to see release notes for previous releases";
-            this.ReleaseNotesLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.ReleaseNotesLinkClicked);
-            // 
-            // UpdaterForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(398, 266);
-            this.Controls.Add(this.ReleaseNotesLink);
-            this.Controls.Add(this.ReleaseDetails);
-            this.Controls.Add(this.StartButton);
-            this.Controls.Add(this.CancelButton);
-            this.Controls.Add(this.ProgressBar);
-            this.Controls.Add(this.StatusLabel);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "UpdaterForm";
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Sledge Updater";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UpdaterFormFormClosing);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			StatusLabel = new System.Windows.Forms.Label();
+			ProgressBar = new System.Windows.Forms.ProgressBar();
+			CancelButton = new System.Windows.Forms.Button();
+			StartButton = new System.Windows.Forms.Button();
+			ReleaseDetails = new System.Windows.Forms.TextBox();
+			ReleaseNotesLink = new System.Windows.Forms.LinkLabel();
+			label1 = new System.Windows.Forms.Label();
+			archBox = new System.Windows.Forms.ComboBox();
+			SuspendLayout();
+			// 
+			// StatusLabel
+			// 
+			StatusLabel.AutoSize = true;
+			StatusLabel.Location = new System.Drawing.Point(14, 10);
+			StatusLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			StatusLabel.Name = "StatusLabel";
+			StatusLabel.Size = new System.Drawing.Size(70, 15);
+			StatusLabel.TabIndex = 0;
+			StatusLabel.Text = "Status Label";
+			// 
+			// ProgressBar
+			// 
+			ProgressBar.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+			ProgressBar.Location = new System.Drawing.Point(14, 284);
+			ProgressBar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			ProgressBar.Name = "ProgressBar";
+			ProgressBar.Size = new System.Drawing.Size(342, 12);
+			ProgressBar.Step = 1;
+			ProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+			ProgressBar.TabIndex = 1;
+			// 
+			// CancelButton
+			// 
+			CancelButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+			CancelButton.Location = new System.Drawing.Point(363, 269);
+			CancelButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			CancelButton.Name = "CancelButton";
+			CancelButton.Size = new System.Drawing.Size(88, 27);
+			CancelButton.TabIndex = 2;
+			CancelButton.Text = "Cancel";
+			CancelButton.UseVisualStyleBackColor = true;
+			CancelButton.Click += CancelButtonClicked;
+			// 
+			// StartButton
+			// 
+			StartButton.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+			StartButton.Location = new System.Drawing.Point(363, 14);
+			StartButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			StartButton.Name = "StartButton";
+			StartButton.Size = new System.Drawing.Size(88, 27);
+			StartButton.TabIndex = 2;
+			StartButton.Text = "Download";
+			StartButton.UseVisualStyleBackColor = true;
+			StartButton.Click += DownloadButtonClicked;
+			// 
+			// ReleaseDetails
+			// 
+			ReleaseDetails.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+			ReleaseDetails.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			ReleaseDetails.Location = new System.Drawing.Point(18, 70);
+			ReleaseDetails.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			ReleaseDetails.Multiline = true;
+			ReleaseDetails.Name = "ReleaseDetails";
+			ReleaseDetails.ReadOnly = true;
+			ReleaseDetails.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			ReleaseDetails.Size = new System.Drawing.Size(432, 173);
+			ReleaseDetails.TabIndex = 3;
+			// 
+			// ReleaseNotesLink
+			// 
+			ReleaseNotesLink.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
+			ReleaseNotesLink.AutoSize = true;
+			ReleaseNotesLink.Location = new System.Drawing.Point(14, 252);
+			ReleaseNotesLink.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			ReleaseNotesLink.Name = "ReleaseNotesLink";
+			ReleaseNotesLink.Size = new System.Drawing.Size(274, 15);
+			ReleaseNotesLink.TabIndex = 7;
+			ReleaseNotesLink.TabStop = true;
+			ReleaseNotesLink.Text = "Click here to see release notes for previous releases";
+			ReleaseNotesLink.LinkClicked += ReleaseNotesLinkClicked;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new System.Drawing.Point(281, 47);
+			label1.Name = "label1";
+			label1.Size = new System.Drawing.Size(75, 15);
+			label1.TabIndex = 8;
+			label1.Text = "Architecture:";
+			// 
+			// archBox
+			// 
+			archBox.FormattingEnabled = true;
+			archBox.Items.AddRange(new object[] { "x86", "x64" });
+			archBox.Location = new System.Drawing.Point(363, 44);
+			archBox.Name = "archBox";
+			archBox.Size = new System.Drawing.Size(87, 23);
+			archBox.TabIndex = 9;
+			archBox.Text = "x86";
+			archBox.SelectedIndexChanged += archBox_SelectedIndexChanged;
+			// 
+			// UpdaterForm
+			// 
+			AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			ClientSize = new System.Drawing.Size(464, 307);
+			Controls.Add(archBox);
+			Controls.Add(label1);
+			Controls.Add(ReleaseNotesLink);
+			Controls.Add(ReleaseDetails);
+			Controls.Add(StartButton);
+			Controls.Add(CancelButton);
+			Controls.Add(ProgressBar);
+			Controls.Add(StatusLabel);
+			FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+			MaximizeBox = false;
+			MinimizeBox = false;
+			Name = "UpdaterForm";
+			ShowInTaskbar = false;
+			StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			Text = "Sledge Updater";
+			FormClosing += UpdaterFormFormClosing;
+			ResumeLayout(false);
+			PerformLayout();
 
-        }
+		}
 
-        #endregion
+		#endregion
 
-        private System.Windows.Forms.Label StatusLabel;
+		private System.Windows.Forms.Label StatusLabel;
         private System.Windows.Forms.ProgressBar ProgressBar;
         private System.Windows.Forms.Button CancelButton;
         private System.Windows.Forms.Button StartButton;
         private System.Windows.Forms.TextBox ReleaseDetails;
         private System.Windows.Forms.LinkLabel ReleaseNotesLink;
-    }
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox archBox;
+	}
 }
 
