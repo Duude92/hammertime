0bfe76c1093315b756a2a87be12edfffb4372d81
Duude92 <switch4acc@gmail.com>
2025-11-14 16:09:27 +0200

    Get release details with specified arch

--- Sledge.Editor/Update/UpdateReleaseDetails.cs
+++ Sledge.Editor/Update/UpdateReleaseDetails.cs
@@ -17,7 +17,7 @@ namespace Sledge.Editor.Update
 
 		public bool Exists => Tag != null;
 
-		public UpdateReleaseDetails(string jsonString, string tag)
+		public UpdateReleaseDetails(string jsonString, string tag, string arch)
 		{
 			var obj = JsonConvert.DeserializeObject(jsonString) as JArray;
 			if (obj == null || obj.Count < 1) return;
@@ -29,7 +29,8 @@ namespace Sledge.Editor.Update
 			var assets = rel?.GetValue("assets") as JArray;
 			if (assets == null || assets.Count < 1) return;
 
-			var exeAsset = assets.FirstOrDefault(x => x is JObject o && o.GetValue("name").ToString().EndsWith(".exe")) as JObject;
+			var exeAsset = assets.FirstOrDefault(x => x is JObject o && o.GetValue("name").ToString().EndsWith($".{arch}.exe")) as JObject;
+			exeAsset = exeAsset ?? assets.FirstOrDefault(x => x is JObject o && o.GetValue("name").ToString().EndsWith(".exe")) as JObject; // Fallback
 			if (exeAsset == null) return;
 
 			Tag = rel.GetValue("tag_name").ToString();
