46e9fa8e76363953a1ea7ef648cd535761d525e6
Duude92 <switch4acc@gmail.com>
2025-12-29 12:51:09 +0200

    fix: entityData become transformable, which allows to transform in single transaction. Transforming in group won't lead to desynchronizing grouped items.

--- Sledge.BspEditor.Tools/Selection/TransformationHandles/RotateTransformHandle.cs
+++ Sledge.BspEditor.Tools/Selection/TransformationHandles/RotateTransformHandle.cs
@@ -59,88 +59,9 @@ namespace Sledge.BspEditor.Tools.Selection.TransformationHandles
 
 		public override void EndDrag(MapDocument document, MapViewport viewport, OrthographicCamera camera, ViewportEvent e, Vector3 position)
 		{
-			var transformation = document.Map.Data.GetOne<TransformationFlags>() ?? new TransformationFlags();
-
-			var selection = document.Selection.Where(x => x is Sledge.BspEditor.Primitives.MapObjects.Entity).OfType<Primitives.MapObjects.Entity>();
-			var tsn = new Transaction();
-
-			foreach (var entity in selection)
-			{
-				if (entity.EntityData.Properties.TryGetValue("angles", out var angleString))
-				{
-
-					var initialAngles = angleString.Split(' ');
-					var initial = NumericsExtensions.Parse(initialAngles[0], initialAngles[1], initialAngles[2], System.Globalization.NumberStyles.Float, CultureInfo.InvariantCulture);
-
-					var origin = camera.ZeroUnusedCoordinate((_rotateStart.Value + _rotateEnd.Value) / 2);
-					if (_origin != null) origin = _origin.Position;
-
-					var forigin = camera.Flatten(origin);
-
-					var origv = Vector3.Normalize(_rotateStart.Value - forigin);
-					var newv = Vector3.Normalize(_rotateEnd.Value - forigin);
-
-					Vector3 previousLocalRotationRadians = new Vector3(
-						MathHelper.DegreesToRadians(initial.X),
-						MathHelper.DegreesToRadians(initial.Y),
-						MathHelper.DegreesToRadians(initial.Z));
-					Matrix4x4 yawMatrix = Matrix4x4.CreateRotationY(-previousLocalRotationRadians.X);
-					Matrix4x4 pitchMatrix = Matrix4x4.CreateRotationX(previousLocalRotationRadians.Z);
-					Matrix4x4 rollMatrix = Matrix4x4.CreateRotationZ(previousLocalRotationRadians.Y);
-
-					Matrix4x4 rotationMatrix = pitchMatrix * yawMatrix * rollMatrix;
-
-					// Now, newLocalRotationDegrees contains the updated local rotation in degrees
-
-					Matrix4x4 viewRotation = Matrix4x4.Identity;
-					if (camera is OrthographicCamera orthographic)
-					{
-
-						var dot = origv.Dot(newv);
-
-						var angle = Math.Acos(Math.Max(-1, Math.Min(1, dot)));
-						if ((origv.Cross(newv).Z < 0)) angle *= -1;
-
-						var roundingDegrees = 15f;
-						if (KeyboardState.Alt) roundingDegrees = 1;
-
-						var deg = angle * (180 / Math.PI);
-						float rnd = (float)(Math.Round(deg / roundingDegrees) * roundingDegrees);
-
-						var anglerad = (float)(rnd * (Math.PI / 180));
-						switch (orthographic.ViewType)
-						{
-							case OrthographicCamera.OrthographicType.Top:
-								viewRotation = Matrix4x4.CreateRotationZ(anglerad);
-								break;
-							case OrthographicCamera.OrthographicType.Front:
-								viewRotation = Matrix4x4.CreateRotationX(anglerad);
-								break;
-							case OrthographicCamera.OrthographicType.Side:
-								viewRotation = Matrix4x4.CreateRotationY(-anglerad);
-								break;
-							default:
-								break;
-						}
-
-						rotationMatrix *= viewRotation;
-						if (transformation.KeepRotationAngle && !entity.Hierarchy.HasChildren)
-						{
-							var newLocalRotationDegrees = MathHelper.ExtractEulerAngles(rotationMatrix);
-
-							var op = new EditEntityDataProperties(entity.ID, new Dictionary<string, string>() {
-						{"angles", $"{Math.Round( MathHelper.RadiansToDegrees( newLocalRotationDegrees.Y))} {Math.Round( MathHelper.RadiansToDegrees( -newLocalRotationDegrees.Z))} {Math.Round(MathHelper.RadiansToDegrees(-newLocalRotationDegrees.X))}" }});
-
-							tsn.Add(op);
-						}
-					}
-				}
-			}
-				((Action)(async () => await MapDocumentOperation.Perform(document, tsn)))();
 			_rotateStart = _rotateEnd = null;
 			base.EndDrag(document, viewport, camera, e, position);
 		}
-
 
 
 		public override void Render(IViewport viewport, OrthographicCamera camera, Vector3 worldMin, Vector3 worldMax, I2DRenderer im)
--- Sledge.BspEditor/Primitives/MapObjectData/EntityData.cs
+++ Sledge.BspEditor/Primitives/MapObjectData/EntityData.cs
@@ -1,119 +1,153 @@
-﻿using System;
+﻿using Sledge.BspEditor.Primitives.MapObjects;
+using Sledge.Common;
+using Sledge.Common.Transport;
+using Sledge.DataStructures.Geometric;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
 using System.Globalization;
 using System.Numerics;
 using System.Runtime.Serialization;
-using Sledge.BspEditor.Primitives.MapObjects;
-using Sledge.Common.Transport;
 
 namespace Sledge.BspEditor.Primitives.MapObjectData
 {
-    public class EntityData : IMapObjectData
-    {
-        public string Name { get; set; }
-        public int Flags { get; set; }
-        public Dictionary<string, string> Properties { get; set; }
+	public class EntityData : IMapObjectData, ITransformable
+	{
+		public string Name { get; set; }
+		public int Flags { get; set; }
+		public Dictionary<string, string> Properties { get; set; }
 
-        public EntityData()
-        {
-            Name = "";
-            Properties = new Dictionary<string, string>();
-        }
+		public EntityData()
+		{
+			Name = "";
+			Properties = new Dictionary<string, string>();
+		}
 
-        public EntityData(SerialisedObject obj)
-        {
-            Name = "";
-            Properties = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
-            foreach (var prop in obj.Properties)
-            {
-                if (prop.Key == "Name") Name = prop.Value;
-                else if (prop.Key == "Flags") Flags = Convert.ToInt32(prop.Value, CultureInfo.InvariantCulture);
-                else Properties[prop.Key] = prop.Value;
-            }
-        }
+		public EntityData(SerialisedObject obj)
+		{
+			Name = "";
+			Properties = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+			foreach (var prop in obj.Properties)
+			{
+				if (prop.Key == "Name") Name = prop.Value;
+				else if (prop.Key == "Flags") Flags = Convert.ToInt32(prop.Value, CultureInfo.InvariantCulture);
+				else Properties[prop.Key] = prop.Value;
+			}
+		}
 
-        [Export(typeof(IMapElementFormatter))]
-        public class ActiveTextureFormatter : StandardMapElementFormatter<EntityData> { }
+		[Export(typeof(IMapElementFormatter))]
+		public class ActiveTextureFormatter : StandardMapElementFormatter<EntityData> { }
 
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            info.AddValue("Properties", Properties);
-        }
+		public void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			info.AddValue("Properties", Properties);
+		}
 
-        public Vector3? GetVector3(string key)
-        {
-            if (!Properties.ContainsKey(key)) return null;
+		public Vector3? GetVector3(string key)
+		{
+			if (!Properties.ContainsKey(key)) return null;
 
-            var spl = (Properties[key] ?? "").Split(new [] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            if (spl.Length < 3) return null;
+			var spl = (Properties[key] ?? "").Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+			if (spl.Length < 3) return null;
 
-            if (float.TryParse(spl[0], NumberStyles.Float, CultureInfo.InvariantCulture, out var x)
-                && float.TryParse(spl[1], NumberStyles.Float, CultureInfo.InvariantCulture, out var y)
-                && float.TryParse(spl[2], NumberStyles.Float, CultureInfo.InvariantCulture, out var z))
-            {
-                return new Vector3(x, y, z);
-            }
-            return null;
-        }
+			if (float.TryParse(spl[0], NumberStyles.Float, CultureInfo.InvariantCulture, out var x)
+				&& float.TryParse(spl[1], NumberStyles.Float, CultureInfo.InvariantCulture, out var y)
+				&& float.TryParse(spl[2], NumberStyles.Float, CultureInfo.InvariantCulture, out var z))
+			{
+				return new Vector3(x, y, z);
+			}
+			return null;
+		}
 
-        public T Get<T>(string key, T defaultValue = default(T))
-        {
-            if (!Properties.ContainsKey(key)) return defaultValue;
-            try
-            {
-                var val = Properties[key];
-                var conv = TypeDescriptor.GetConverter(typeof(T));
-                return (T)conv.ConvertFromString(null, CultureInfo.InvariantCulture, val);
-            }
-            catch
-            {
-                return defaultValue;
-            }
-        }
-        public string GetStringProperty(string key, string defaultValue)
-        {
+		public T Get<T>(string key, T defaultValue = default(T))
+		{
 			if (!Properties.ContainsKey(key)) return defaultValue;
-            return Properties[key];
+			try
+			{
+				var val = Properties[key];
+				var conv = TypeDescriptor.GetConverter(typeof(T));
+				return (T)conv.ConvertFromString(null, CultureInfo.InvariantCulture, val);
+			}
+			catch
+			{
+				return defaultValue;
+			}
+		}
+		public string GetStringProperty(string key, string defaultValue)
+		{
+			if (!Properties.ContainsKey(key)) return defaultValue;
+			return Properties[key];
 		}
 
 		public void Set<T>(string key, T value)
-        {
-            var conv = TypeDescriptor.GetConverter(typeof(T));
-            var v = conv.ConvertToString(null, CultureInfo.InvariantCulture, value);
-            Properties[key] = v;
-        }
+		{
+			var conv = TypeDescriptor.GetConverter(typeof(T));
+			var v = conv.ConvertToString(null, CultureInfo.InvariantCulture, value);
+			Properties[key] = v;
+		}
 
-        public void Unset(string key)
-        {
-            if (Properties.ContainsKey(key)) Properties.Remove(key);
-        }
+		public void Unset(string key)
+		{
+			if (Properties.ContainsKey(key)) Properties.Remove(key);
+		}
 
-        public IMapElement Clone()
-        {
-            var ed = new EntityData();
-            ed.Name = Name;
-            ed.Flags = Flags;
-            ed.Properties = new Dictionary<string, string>(Properties);
-            return ed;
-        }
+		public IMapElement Clone()
+		{
+			var ed = new EntityData();
+			ed.Name = Name;
+			ed.Flags = Flags;
+			ed.Properties = new Dictionary<string, string>(Properties);
+			return ed;
+		}
 
-        public IMapElement Copy(UniqueNumberGenerator numberGenerator)
-        {
-            return Clone();
-        }
+		public IMapElement Copy(UniqueNumberGenerator numberGenerator)
+		{
+			return Clone();
+		}
 
-        public SerialisedObject ToSerialisedObject()
-        {
-            var so = new SerialisedObject("EntityData");
-            foreach (var p in Properties)
-            {
-                so.Set(p.Key, p.Value);
-            }
-            so.Set("Name", Name);
-            so.Set("Flags", Flags);
-            return so;
-        }
-    }
+		public SerialisedObject ToSerialisedObject()
+		{
+			var so = new SerialisedObject("EntityData");
+			foreach (var p in Properties)
+			{
+				so.Set(p.Key, p.Value);
+			}
+			so.Set("Name", Name);
+			so.Set("Flags", Flags);
+			return so;
+		}
+
+		public void Transform(Matrix4x4 matrix)
+		{
+			if (Properties.TryGetValue("angles", out var angleString))
+			{
+				var initialAngles = angleString.Split(' ');
+				var initial = NumericsExtensions.Parse(initialAngles[0], initialAngles[1], initialAngles[2], System.Globalization.NumberStyles.Float, CultureInfo.InvariantCulture);
+
+				Vector3 previousLocalRotationRadians = new Vector3(
+					MathHelper.DegreesToRadians(initial.X),
+					MathHelper.DegreesToRadians(initial.Y),
+					MathHelper.DegreesToRadians(initial.Z));
+				Matrix4x4 yawMatrix = Matrix4x4.CreateRotationY(-previousLocalRotationRadians.X);
+				Matrix4x4 pitchMatrix = Matrix4x4.CreateRotationX(previousLocalRotationRadians.Z);
+				Matrix4x4 rollMatrix = Matrix4x4.CreateRotationZ(previousLocalRotationRadians.Y);
+
+				Matrix4x4 rotationMatrix = pitchMatrix * yawMatrix * rollMatrix;
+
+				rotationMatrix *= matrix;
+
+				Vector3 deltaEuler = MathHelper.ExtractEulerAngles(rotationMatrix);
+
+				var addition = new Vector3(
+					MathHelper.RadiansToDegrees(deltaEuler.Y),
+					MathHelper.RadiansToDegrees(-deltaEuler.Z),
+					MathHelper.RadiansToDegrees(-deltaEuler.X));
+
+
+				Properties["angles"] =
+					$"{Math.Round(addition.X)} {Math.Round(addition.Y)} {Math.Round(addition.Z)}";
+			}
+		}
+	}
 }
