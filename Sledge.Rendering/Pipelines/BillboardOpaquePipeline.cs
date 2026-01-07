824bfd363e7a4f7636b1aee80934090c83ef6fd9
Duude92 <switch4acc@gmail.com>
2026-01-07 19:25:09 +0200

    fix: billboard opaque pipeline layout

--- Sledge.Rendering/Pipelines/BillboardOpaquePipeline.cs
+++ Sledge.Rendering/Pipelines/BillboardOpaquePipeline.cs
@@ -9,97 +9,104 @@ using Veldrid;
 
 namespace Sledge.Rendering.Pipelines
 {
-    public class BillboardOpaquePipeline : IPipeline
-    {
-        public PipelineType Type => PipelineType.BillboardOpaque;
-        public PipelineGroup Group => PipelineGroup.Opaque;
-        public float Order => 6;
+	public class BillboardOpaquePipeline : IPipeline
+	{
+		public PipelineType Type => PipelineType.BillboardOpaque;
+		public PipelineGroup Group => PipelineGroup.Opaque;
+		public float Order => 6;
 
-        private Shader _vertex;
-        private Shader _geometry;
-        private Shader _fragment;
-        private Pipeline _pipeline;
-        private DeviceBuffer _projectionBuffer;
-        private ResourceSet _projectionResourceSet;
+		private Shader _vertex;
+		private Shader _geometry;
+		private Shader _fragment;
+		private Pipeline _pipeline;
+		private DeviceBuffer _projectionBuffer;
+		private ResourceSet _projectionResourceSet;
+		private ResourceLayout _uvLayout;
 
-        public void Create(RenderContext context, TextureSampleCount sampleCount)
-        {
-            (_vertex, _geometry, _fragment) = context.ResourceLoader.LoadShadersGeometry("Billboard");
 
-            var pDesc = new GraphicsPipelineDescription
-            {
-                BlendState = BlendStateDescription.SingleAlphaBlend,
-                DepthStencilState = DepthStencilStateDescription.DepthOnlyLessEqual,
-                RasterizerState = RasterizerStateDescription.Default,
-                PrimitiveTopology = PrimitiveTopology.PointList,
-                ResourceLayouts = new[] { context.ResourceLoader.ProjectionLayout, context.ResourceLoader.TextureLayout },
-                ShaderSet = new ShaderSetDescription(new[] { context.ResourceLoader.VertexStandardLayoutDescription }, new[] { _vertex, _geometry, _fragment }),
-                Outputs = new OutputDescription
-                {
-                    ColorAttachments = new[] { new OutputAttachmentDescription(PixelFormat.B8_G8_R8_A8_UNorm) },
-                    DepthAttachment = new OutputAttachmentDescription(PixelFormat.R32_Float),
-                    SampleCount = sampleCount
-                }
-            };
+		public void Create(RenderContext context, TextureSampleCount sampleCount)
+		{
+			(_vertex, _geometry, _fragment) = context.ResourceLoader.LoadShadersGeometry("Billboard");
+			_uvLayout = context.Device.ResourceFactory.CreateResourceLayout(new ResourceLayoutDescription(
+			new ResourceLayoutElementDescription("UVS", ResourceKind.UniformBuffer, ShaderStages.Geometry)));
 
-            _pipeline = context.Device.ResourceFactory.CreateGraphicsPipeline(ref pDesc);
+			var pDesc = new GraphicsPipelineDescription
+			{
+				BlendState = BlendStateDescription.SingleAlphaBlend,
+				DepthStencilState = DepthStencilStateDescription.DepthOnlyLessEqual,
+				RasterizerState = RasterizerStateDescription.Default,
+				PrimitiveTopology = PrimitiveTopology.PointList,
+				ResourceLayouts = new[] { context.ResourceLoader.ProjectionLayout,
+					_uvLayout,
+					context.ResourceLoader.TextureLayout },
+				ShaderSet = new ShaderSetDescription(new[] { context.ResourceLoader.VertexStandardLayoutDescription }, new[] { _vertex, _geometry, _fragment }),
+				Outputs = new OutputDescription
+				{
+					ColorAttachments = new[] { new OutputAttachmentDescription(PixelFormat.B8_G8_R8_A8_UNorm) },
+					DepthAttachment = new OutputAttachmentDescription(PixelFormat.R32_Float),
+					SampleCount = sampleCount
+				}
+			};
 
-            _projectionBuffer = context.Device.ResourceFactory.CreateBuffer(
-                new BufferDescription((uint)Unsafe.SizeOf<UniformProjection>(), BufferUsage.UniformBuffer)
-            );
+			_pipeline = context.Device.ResourceFactory.CreateGraphicsPipeline(ref pDesc);
 
-            _projectionResourceSet = context.Device.ResourceFactory.CreateResourceSet(
+			_projectionBuffer = context.Device.ResourceFactory.CreateBuffer(
+				new BufferDescription((uint)Unsafe.SizeOf<UniformProjection>(), BufferUsage.UniformBuffer)
+			);
+
+			_projectionResourceSet = context.Device.ResourceFactory.CreateResourceSet(
                 new ResourceSetDescription(context.ResourceLoader.ProjectionLayout, _projectionBuffer)
-            );
-        }
+			);
+		}
 
-        public void SetupFrame(RenderContext context, Engine.Engine.ViewProjectionBuffer viewProjectionBuffer)
-        {
-            var view = viewProjectionBuffer.View;
-            if (!Matrix4x4.Invert(view, out var invView)) invView = Matrix4x4.Identity;
+		public void SetupFrame(RenderContext context, Engine.Engine.ViewProjectionBuffer viewProjectionBuffer)
+		{
+			var view = viewProjectionBuffer.View;
+			if (!Matrix4x4.Invert(view, out var invView)) invView = Matrix4x4.Identity;
 
-            context.Device.UpdateBuffer(_projectionBuffer, 0, new UniformProjection
-            {
-                Selective = context.SelectiveTransform,
-                Model = invView,
-                View = view,
-                Projection = viewProjectionBuffer.Projection
-            });
-        }
+			context.Device.UpdateBuffer(_projectionBuffer, 0, new UniformProjection
+			{
+				Selective = context.SelectiveTransform,
+				Model = invView,
+				View = view,
+				Projection = viewProjectionBuffer.Projection
+			});
+		}
 
-        public void Render(RenderContext context, IViewport target, CommandList cl, IEnumerable<IRenderable> renderables)
-        {
-            cl.SetPipeline(_pipeline);
-            cl.SetGraphicsResourceSet(0, _projectionResourceSet);
+		public void Render(RenderContext context, IViewport target, CommandList cl, IEnumerable<IRenderable> renderables)
+		{
+			cl.SetPipeline(_pipeline);
+			cl.SetGraphicsResourceSet(0, _projectionResourceSet);
 
-            foreach (var r in renderables)
-            {
-                r.Render(context, this, target, cl);
-            }
-        }
+			foreach (var r in renderables)
+			{
+				r.Render(context, this, target, cl);
+			}
+		}
 
-        public void Render(RenderContext context, IViewport target, CommandList cl, IRenderable renderable, ILocation locationObject)
-        {
-            cl.SetPipeline(_pipeline);
-            cl.SetGraphicsResourceSet(0, _projectionResourceSet);
+		public void Render(RenderContext context, IViewport target, CommandList cl, IRenderable renderable, ILocation locationObject)
+		{
+			cl.SetPipeline(_pipeline);
+			cl.SetGraphicsResourceSet(0, _projectionResourceSet);
 
-            renderable.Render(context, this, target, cl, locationObject);
-        }
+			renderable.Render(context, this, target, cl, locationObject);
+		}
 
-        public void Bind(RenderContext context, CommandList cl, string binding)
-        {
-            var tex = context.ResourceLoader.GetTexture(binding);
-            tex?.BindTo(cl, 1);
-        }
+		public void Bind(RenderContext context, CommandList cl, string binding)
+		{
+			var tex = context.ResourceLoader.GetTexture(binding);
+			tex?.BindTo(cl, 1);
+		}
 
-        public void Dispose()
-        {
-            _projectionResourceSet?.Dispose();
-            _projectionBuffer?.Dispose();
-            _pipeline?.Dispose();
-            _vertex?.Dispose();
-            _geometry?.Dispose();
-            _fragment?.Dispose();
-        }
-    }
+		public void Dispose()
+		{
+			_projectionResourceSet?.Dispose();
+			_uvLayout?.Dispose();
+			_projectionBuffer?.Dispose();
+			_pipeline?.Dispose();
+			_vertex?.Dispose();
+			_geometry?.Dispose();
+			_fragment?.Dispose();
+		}
+	}
 }
