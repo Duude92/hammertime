2b42b8813d34138f8a69075f724cdfe56d56a5a1
Duude92 <switch4acc@gmail.com>
2026-01-14 19:36:23 +0200

    chore: remove windows forms viewport

--- Sledge.Rendering/Viewports/IViewport.cs
+++ Sledge.Rendering/Viewports/IViewport.cs
@@ -1,30 +1,10 @@
 ﻿using System;
-using System.Windows.Forms;
 using Sledge.Rendering.Cameras;
 using Sledge.Rendering.Overlay;
 using Veldrid;
 
 namespace Sledge.Rendering.Viewports
 {
-    public interface IViewportWF : IRenderTarget
-    {
-        int ID { get; }
-
-        int Width { get; }
-        int Height { get; }
-        Control Control { get; }
-        bool IsFocused { get; }
-
-        ICamera Camera { get; set; }
-        ViewportOverlay Overlay { get; }
-        Framebuffer ViewportFramebuffer { get; }
-		public Resources.Texture ViewportRenderTexture { get; }
-		void InitFramebuffer(TextureSampleCount sampleCount);
-        void ResolveRenderTexture(CommandList commandList);
-
-		void Update(long frame);
-        event EventHandler<long> OnUpdate;
-    }
 	public interface IViewport : IRenderTarget
 	{
 		int ID { get; }
