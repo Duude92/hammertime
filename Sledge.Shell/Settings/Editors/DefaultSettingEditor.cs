8929e55e48e78287b168b5dc2fd3368910d2fabf
Duude92 <switch4acc@gmail.com>
2026-01-17 18:43:06 +0200

    feat: replace defaultsetting with avalonia

--- Sledge.Shell/Settings/Editors/DefaultSettingEditor.cs
+++ Sledge.Shell/Settings/Editors/DefaultSettingEditor.cs
@@ -1,31 +1,36 @@
 ﻿using System;
 using System.Drawing;
-using System.Windows.Forms;
+using Avalonia.Controls;
 using Sledge.Common.Shell.Settings;
 
 namespace Sledge.Shell.Settings.Editors
 {
-    public class DefaultSettingEditor : UserControl, ISettingEditor
-    {
-        public event EventHandler<SettingKey> OnValueChanged;
-        public string Label { get; set; }
+	public class DefaultSettingEditor : UserControl, ISettingEditor
+	{
+		public event EventHandler<SettingKey> OnValueChanged;
+		public string Label { get; set; }
 
-        public object Value
-        {
-            get => _box.Text;
-            set => _box.Text = Convert.ToString(value);
-        }
+		public object Value
+		{
+			get => _box.Text;
+			set => _box.Text = Convert.ToString(value);
+		}
 
-        public object Control => this;
-        public SettingKey Key { get; set; }
+		public object Control => this;
+		public SettingKey Key { get; set; }
 
-        private readonly TextBox _box;
+		private readonly TextBox _box;
 
-        public DefaultSettingEditor()
-        {
-            Size = new Size(400, 30);
-            _box = new TextBox();
-            Controls.Add(_box);
-        }
-    }
+		public DefaultSettingEditor()
+		{
+			Width = 400; Height = 30;
+			_box = new TextBox();
+			Content = _box;
+		}
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
